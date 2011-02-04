0aab64fa614e273f2958825e852186d3e7cd425b
Ralf Quast <ralf.quast@brockmann-consult.de>
2011-02-04 18:03:53 +0100

    Sea ice concentration reader test

--- src/test/java/org/esa/cci/sst/reader/SeaIceObservationReaderTest.java
+++ src/test/java/org/esa/cci/sst/reader/SeaIceObservationReaderTest.java
@@ -1,12 +1,48 @@
 package org.esa.cci.sst.reader;
 
 import org.junit.Test;
+import ucar.ma2.Section;
+import ucar.ma2.StructureData;
+import ucar.nc2.Attribute;
 import ucar.nc2.NetcdfFile;
+import ucar.nc2.Structure;
+import ucar.nc2.Variable;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class SeaIceObservationReaderTest {
+
+    private static final File TEST_FILE = new File("testdata/SeaIceConc", "ice_conc_sh_qual_201006301200.hdf");
+
     @Test
     public void testReadObservation() throws IOException {
+        assertTrue(NetcdfFile.canOpen(TEST_FILE.getPath()));
+
+        final NetcdfFile ncFile = NetcdfFile.open(TEST_FILE.getPath());
+        assertNotNull(ncFile);
+
+        final List<Variable> variableList = ncFile.getVariables();
+        assertFalse(variableList.isEmpty());
+        for (Variable v : variableList) {
+            System.out.println("v.getName() = " + v.getName());
+        }
+
+        final Variable header = ncFile.findVariable("Header");
+        assertTrue(header instanceof Structure);
+        final List<Variable> headerVariables = ((Structure) header).getVariables();
+        for (Variable v : headerVariables) {
+            System.out.println("v.getName() = " + v.getName());
+        }
+        final List<Attribute> attributeList = header.getAttributes();
+        for (Attribute a : attributeList) {
+            System.out.println("a.getName() = " + a.getName());
+            System.out.println("a.getValue() = " + a.getValue(0));
+        }
+        final Variable data = ncFile.findVariable("Data/data[00]");
+        assertNotNull(data);
     }
 }
