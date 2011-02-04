731ff76956d97c7b708ce13ccfdd4aa84148d535
Ralf Quast <ralf.quast@brockmann-consult.de>
2011-02-04 11:59:03 +0100

    Fix for geo-boundary.

--- src/main/java/org/esa/cci/sst/reader/MetopMatchupReader.java
+++ src/main/java/org/esa/cci/sst/reader/MetopMatchupReader.java
@@ -2,6 +2,7 @@ package org.esa.cci.sst.reader;
 
 import org.esa.cci.sst.data.DataFile;
 import org.esa.cci.sst.data.Observation;
+import org.esa.cci.sst.util.PgUtil;
 import org.esa.cci.sst.util.TimeUtil;
 import org.postgis.LinearRing;
 import org.postgis.PGgeometry;
@@ -12,7 +13,10 @@ import ucar.ma2.InvalidRangeException;
 import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Reads records from an METOP MD NetCDF input file and creates Observations.
@@ -24,6 +28,7 @@ import java.util.Date;
  */
 public class MetopMatchupReader extends NetcdfMatchupReader {
 
+    private static final int LAT_LON_FILL_VALUE = -32768;
     static final long MILLISECONDS_1981;
 
     static {
@@ -34,8 +39,8 @@ public class MetopMatchupReader extends NetcdfMatchupReader {
         }
     }
 
-    protected int noOfLines;
-    protected int noOfColumns;
+    protected int rowCount;
+    protected int colCount;
 
     @Override
     public String getDimensionName() {
@@ -49,7 +54,7 @@ public class MetopMatchupReader extends NetcdfMatchupReader {
 
     @Override
     public String[] getVariableNames() {
-        return new String[] {
+        return new String[]{
                 "msr_id",
                 "lat",
                 "lon",
@@ -64,15 +69,14 @@ public class MetopMatchupReader extends NetcdfMatchupReader {
     @Override
     public void init(File observationFile, DataFile dataFileEntry) throws IOException {
         super.init(observationFile, dataFileEntry);
-        noOfLines = netcdf.findDimension("ny").getLength();
-        noOfColumns = netcdf.findDimension("nx").getLength();
+        rowCount = netcdf.findDimension("ny").getLength();
+        colCount = netcdf.findDimension("nx").getLength();
     }
 
     @Override
     public long getTime(int recordNo) throws IOException, InvalidRangeException {
-        //int line = getShort("box_center_y_coord", recordNo);
-        int line = noOfLines / 2;
-        return MILLISECONDS_1981 + (long) ((getDouble("msr_time", recordNo) + getDouble("dtime", recordNo, line)) * 1000);
+        return MILLISECONDS_1981 + 1000L * Math.round(
+                getDouble("msr_time", recordNo) + getDouble("dtime", recordNo, rowCount / 2));
     }
 
     /**
@@ -81,37 +85,26 @@ public class MetopMatchupReader extends NetcdfMatchupReader {
      * from  left to right looking in flight direction.
      *
      * @param recordNo index in observation file, must be between 0 and less than numRecords
+     *
      * @return Observation for METOP sub-scene
-     * @throws IOException  if file io fails
-     * @throws InvalidRangeException  if record number is out of range 0 .. numRecords-1
+     *
+     * @throws IOException           if file io fails
+     * @throws InvalidRangeException if record number is out of range 0 .. numRecords-1
      */
     @Override
     public Observation readObservation(int recordNo) throws IOException, InvalidRangeException {
-
-        //int    line   = getShort("box_center_y_coord", recordNo);
-        //int    column = getShort("box_center_x_coord", recordNo);
-        int line = noOfLines / 2;
-        int column = noOfColumns / 2;
+        final int x = colCount / 2;
+        final int y = rowCount / 2;
 
         final Observation observation = new Observation();
         observation.setName(getString("msr_id", recordNo));
         observation.setSensor("metop");
-        observation.setLocation(new PGgeometry(new Polygon(new LinearRing[] { new LinearRing(new Point[] {
-                new Point(coordinateOf(getShort("lon", recordNo, 0, 0)),
-                          coordinateOf(getShort("lat", recordNo, 0, 0))),
-                new Point(coordinateOf(getShort("lon", recordNo, 0, noOfColumns - 1)),
-                          coordinateOf(getShort("lat", recordNo, 0, noOfColumns - 1))),
-                new Point(coordinateOf(getShort("lon", recordNo, noOfLines - 1, noOfColumns - 1)),
-                          coordinateOf(getShort("lat", recordNo, noOfLines - 1, noOfColumns - 1))),
-                new Point(coordinateOf(getShort("lon", recordNo, noOfLines - 1, 0)),
-                          coordinateOf(getShort("lat", recordNo, noOfLines - 1, 0))),
-                new Point(coordinateOf(getShort("lon", recordNo, 0, 0)),
-                          coordinateOf(getShort("lat", recordNo, 0, 0)))
-        })})));
-        observation.setTime(dateOf(getDouble("msr_time", recordNo) + getDouble("dtime", recordNo, line)));
+        observation.setLocation(new PGgeometry(new Polygon(new LinearRing[]{new LinearRing(getPoints(recordNo))})));
+        observation.setTime(toDate(getDouble("msr_time", recordNo) + getDouble("dtime", recordNo, y)));
         observation.setDatafile(dataFileEntry);
         observation.setRecordNo(recordNo);
-        observation.setClearSky(getShort(getSstVariableName(), recordNo, line, column) != sstFillValue);
+        observation.setClearSky(getShort(getSstVariableName(), recordNo, y, x) != sstFillValue);
+
         return observation;
     }
 
@@ -120,39 +113,101 @@ public class MetopMatchupReader extends NetcdfMatchupReader {
      * may serve as reference observation in some matchup.
      *
      * @param recordNo index in observation file, must be between 0 and less than numRecords
+     *
      * @return Observation for METOP pixel
-     * @throws IOException  if file io fails
-     * @throws InvalidRangeException  if record number is out of range 0 .. numRecords-1
+     *
+     * @throws IOException           if file io fails
+     * @throws InvalidRangeException if record number is out of range 0 .. numRecords-1
      */
     @Override
     public Observation readRefObs(int recordNo) throws IOException, InvalidRangeException {
-
-        //int    line   = getShort("box_center_y_coord", recordNo);
-        //int    column = getShort("box_center_x_coord", recordNo);
-        int line = noOfLines / 2;
-        int column = noOfColumns / 2;
+        final int x = colCount / 2;
+        final int y = rowCount / 2;
 
         final Observation observation = new Observation();
         observation.setName(getString("msr_id", recordNo));
         observation.setSensor("metop.ref");
-        observation.setLocation(new PGgeometry(new Point(coordinateOf(getShort("lon", recordNo, line, column)),
-                                                         coordinateOf(getShort("lat", recordNo, line, column)))));
-        observation.setTime(dateOf(getDouble("msr_time", recordNo) + getDouble("dtime", recordNo, line)));
+        observation.setLocation(new PGgeometry(newPoint(getLon(recordNo, y, x), getLat(recordNo, y, x))));
+        observation.setTime(toDate(getDouble("msr_time", recordNo) + getDouble("dtime", recordNo, y)));
         observation.setDatafile(dataFileEntry);
         observation.setRecordNo(recordNo);
-        observation.setClearSky(getShort(getSstVariableName(), recordNo, line, column) != sstFillValue);
+        observation.setClearSky(getShort(getSstVariableName(), recordNo, y, x) != sstFillValue);
+
         return observation;
     }
 
-    private Date dateOf(double secondsSince1981) {
+    private Point[] getPoints(int recordNo) throws IOException, InvalidRangeException {
+        final List<Point> pointList = new ArrayList<Point>(9);
+
+        for (int i = 0; i < rowCount - 1; i++) {
+            final int lon = getLon(recordNo, i, 0);
+            final int lat = getLat(recordNo, i, 0);
+            if (isValid(lon, lat)) {
+                if (pointList.size() < 1) {
+                    pointList.add(newPoint(lon, lat));
+                } else {
+                    pointList.add(1, newPoint(lon, lat));
+                }
+            }
+        }
+        for (int k = 0; k < colCount - 1; k++) {
+            final int lon = getLon(recordNo, rowCount - 1, k);
+            final int lat = getLat(recordNo, rowCount - 1, k);
+            if (isValid(lon, lat)) {
+                if (pointList.size() < 3) {
+                    pointList.add(newPoint(lon, lat));
+                } else {
+                    pointList.add(3, newPoint(lon, lat));
+                }
+            }
+        }
+        for (int i = rowCount - 1; i > 0; i--) {
+            final int lon = getLon(recordNo, i, colCount - 1);
+            final int lat = getLat(recordNo, i, colCount - 1);
+            if (isValid(lon, lat)) {
+                if (pointList.size() < 5) {
+                    pointList.add(newPoint(lon, lat));
+                } else {
+                    pointList.add(5, newPoint(lon, lat));
+                }
+            }
+        }
+        for (int k = colCount - 1; k > 0; k--) {
+            final int lon = getLon(recordNo, 0, k);
+            final int lat = getLat(recordNo, 0, k);
+            if (isValid(lon, lat)) {
+                if (pointList.size() < 7) {
+                    pointList.add(newPoint(lon, lat));
+                } else {
+                    pointList.add(7, newPoint(lon, lat));
+                }
+            }
+        }
+        pointList.add(pointList.get(0));
+        if (PgUtil.isClockwise(pointList)) {
+            Collections.reverse(pointList);
+        }
+
+        return pointList.toArray(new Point[pointList.size()]);
+    }
+
+    private int getLat(int recordNo, int y, int x) throws IOException, InvalidRangeException {
+        return getShort("lat", recordNo, y, x);
+    }
+
+    private int getLon(int recordNo, int y, int x) throws IOException, InvalidRangeException {
+        return getShort("lon", recordNo, y, x);
+    }
+
+    private static Date toDate(double secondsSince1981) {
         return new Date(MILLISECONDS_1981 + (long) secondsSince1981 * 1000);
     }
 
-    // TODO handle fill value
-    private float coordinateOf(int shortCoordinate) {
-        if (shortCoordinate == -32768) {
-            throw new IllegalArgumentException("TODO handle fill value");
-        }
-        return shortCoordinate * 0.01f;
+    private static boolean isValid(int lon, int lat) {
+        return lon != LAT_LON_FILL_VALUE && lat != LAT_LON_FILL_VALUE;
+    }
+
+    private static Point newPoint(int lon, int lat) {
+        return new Point(0.01 * lon, 0.01 * lat);
     }
 }
