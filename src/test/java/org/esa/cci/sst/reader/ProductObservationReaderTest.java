37ad591eb3c97eb58590cb7e3579198303e0f2e9
Ralf Quast <ralf.quast@brockmann-consult.de>
2011-02-11 15:20:58 +0100

    Added reading of AAI product to test.

--- src/test/java/org/esa/cci/sst/reader/ProductObservationReaderTest.java
+++ src/test/java/org/esa/cci/sst/reader/ProductObservationReaderTest.java
@@ -2,33 +2,40 @@ package org.esa.cci.sst.reader;
 
 import org.esa.cci.sst.data.DataFile;
 import org.esa.cci.sst.data.Observation;
+import org.esa.cci.sst.util.PgUtil;
 import org.esa.cci.sst.util.TimeUtil;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.After;
 import org.junit.Test;
+import org.postgis.Geometry;
+import org.postgis.Point;
 import ucar.ma2.InvalidRangeException;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class ProductObservationReaderTest {
 
-    private static final File AMSRE_TEST_FILE =
-            new File("testdata/AMSR-E/152", "20100601-AMSRE-REMSS-L2P-amsr_l2b_v05_r42958.dat-v01.nc");
+    private static final String AAI_RESOURCE_NAME = "20100601.egr";
+    private static final String AMSRE_RESOURCE_NAME = "20100601-AMSRE-REMSS-L2P-amsr_l2b_v05_r42958.dat-v01.nc.gz";
+
     private static DataFile dataFile;
     private static ObservationReader reader;
 
-    @BeforeClass
-    public static void init() throws IOException {
+    public void init(File file) throws IOException {
         dataFile = new DataFile();
-        reader = new ProductObservationReader("");
-        reader.init(AMSRE_TEST_FILE, dataFile);
+        reader = new ProductObservationReader("any");
+        reader.init(file, dataFile);
     }
 
-    @AfterClass
-    public static void clean() {
+    @After
+    public void clean() {
         try {
             reader.close();
         } catch (IOException e) {
@@ -37,21 +44,62 @@ public class ProductObservationReaderTest {
     }
 
     @Test
-    public void testGetNumRecords() {
+    public void testGetNumRecords() throws URISyntaxException, IOException {
+        init(getResourceAsFile(AMSRE_RESOURCE_NAME));
+        assertEquals(1, reader.getNumRecords());
+        clean();
+
+        init(getResourceAsFile(AAI_RESOURCE_NAME));
         assertEquals(1, reader.getNumRecords());
     }
 
     @Test
-    public void testGetTime() throws IOException, InvalidRangeException {
+    public void testGetTime() throws IOException, InvalidRangeException, URISyntaxException {
+        init(getResourceAsFile(AMSRE_RESOURCE_NAME));
+        assertTrue(reader.getTime(0) > TimeUtil.MILLISECONDS_1981);
+        clean();
+
+        init(getResourceAsFile(AAI_RESOURCE_NAME));
         assertTrue(reader.getTime(0) > TimeUtil.MILLISECONDS_1981);
     }
 
     @Test
-    public void testReadObservation() throws IOException, InvalidRangeException {
-        final Observation observation = reader.readObservation(0);
+    public void testReadObservation() throws IOException, InvalidRangeException, URISyntaxException {
+        Geometry geometry;
+        Observation observation;
+
+        init(getResourceAsFile(AMSRE_RESOURCE_NAME));
+        observation = reader.readObservation(0);
         assertSame(dataFile, observation.getDatafile());
         assertEquals(reader.getTime(0), observation.getTime().getTime());
         assertNotNull(observation.getLocation());
-        assertTrue(observation.getLocation().getGeometry().checkConsistency());
+        geometry = observation.getLocation().getGeometry();
+        assertTrue(geometry.checkConsistency());
+        assertFalse(PgUtil.isClockwise(getPoints(geometry)));
+        clean();
+
+        init(getResourceAsFile(AAI_RESOURCE_NAME));
+        observation = reader.readObservation(0);
+        assertSame(dataFile, observation.getDatafile());
+        assertEquals(reader.getTime(0), observation.getTime().getTime());
+        assertNotNull(observation.getLocation());
+        geometry = observation.getLocation().getGeometry();
+        assertTrue(geometry.checkConsistency());
+        assertFalse(PgUtil.isClockwise(getPoints(geometry)));
+    }
+
+    private static List<Point> getPoints(Geometry geometry) {
+        final List<Point> pointList = new ArrayList<Point>(geometry.numPoints());
+        for (int i = 0; i < geometry.numPoints(); i++) {
+            pointList.add(geometry.getPoint(i));
+        }
+        return pointList;
+    }
+
+    private static File getResourceAsFile(String name) throws URISyntaxException {
+        final URL url = AaiProductReaderTest.class.getResource(name);
+        final URI uri = url.toURI();
+
+        return new File(uri);
     }
 }
