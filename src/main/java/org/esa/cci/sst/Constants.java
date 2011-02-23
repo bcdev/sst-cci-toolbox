8252c8a3b63aa9950919bdf01fe7c60aab4ce3d8
Ralf Quast <ralf.quast@brockmann-consult.de>
2011-02-23 11:56:17 +0100

    AVHRR GAC constants

--- src/main/java/org/esa/cci/sst/Constants.java
+++ src/main/java/org/esa/cci/sst/Constants.java
@@ -6,9 +6,10 @@ public class Constants {
     public static final String DATA_SCHEMA_NAME_METOP_MD = "metop-md";
     public static final String DATA_SCHEMA_NAME_SEVIRI_MD = "seviri-md";
 
+    public static final String DATA_SCHEMA_NAME_AAI = "aai";
     public static final String DATA_SCHEMA_NAME_ATSR = "atsr";
     public static final String DATA_SCHEMA_NAME_AMR = "amsre";
-    public static final String DATA_SCHEMA_NAME_AAI = "aai";
+    public static final String DATA_SCHEMA_NAME_AVHRR_GAC = "avhrr_gac";
     public static final String DATA_SCHEMA_NAME_TMI = "tmi";
 
     public static final String SENSOR_NAME_AATSR_REFERENCE = "aatsr-md-ref";
@@ -16,6 +17,7 @@ public class Constants {
 
     public static final String SENSOR_NAME_AAI = "aai";
     public static final String SENSOR_NAME_AATSR = "aatsr";
+    public static final String SENSOR_NAME_AVHRR = "avhrr";
     public static final String SENSOR_NAME_TMI = "tmi";
     public static final String SENSOR_NAME_AMSRE = "amsre";
     public static final String SENSOR_NAME_METOP = "metop";
