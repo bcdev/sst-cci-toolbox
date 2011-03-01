caf466dec2b029f9ee7ed43ea974a071d52e3dc0
Ralf Quast <ralf.quast@brockmann-consult.de>
2011-03-01 10:56:53 +0100

    Static loading of java agent. Needs maven repository path extracted to properties file or environment variable.

--- src/test/java/org/esa/cci/sst/util/MmdFormatGeneratorTest.java
+++ src/test/java/org/esa/cci/sst/util/MmdFormatGeneratorTest.java
@@ -16,26 +16,48 @@
 
 package org.esa.cci.sst.util;
 
+import com.sun.tools.attach.AgentInitializationException;
+import com.sun.tools.attach.AgentLoadException;
+import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import ucar.ma2.DataType;
 import ucar.nc2.NetcdfFileWriteable;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * TODO fill out or delete
  *
  * @author Thomas Storm
  */
+@Ignore
 public class MmdFormatGeneratorTest {
+
+    @BeforeClass
+    public static void loadAgent() throws IOException, AttachNotSupportedException, AgentInitializationException,
+                                     AgentLoadException {
+        String nameOfRunningVM = ManagementFactory.getRuntimeMXBean().getName();
+        int p = nameOfRunningVM.indexOf('@');
+        String pid = nameOfRunningVM.substring(0, p);
+
+        VirtualMachine vm = VirtualMachine.attach(pid);
+        vm.loadAgent("/Users/ralf/Public/repository/org/apache/openjpa/openjpa-all/2.0.0/openjpa-all-2.0.0.jar");
+        vm.detach();
+    }
 
     @Test
     public void testAddContent() throws Exception {
         final MmdFormatGenerator generator = new MmdFormatGenerator();
         final NetcdfFileWriteable file = NetcdfFileWriteable.createNew("");
-        file.addDimension("matchup", 0, false, true, true);
+        file.addDimension(MmdFormatGenerator.DIMENSION_NAME_MATCHUP, 0, false, true, true);
         final String var1 = "var1";
-        file.addVariable(var1, DataType.LONG, "matchup");
+        file.addVariable(var1, DataType.LONG, MmdFormatGenerator.DIMENSION_NAME_MATCHUP);
         generator.addContent(file);
         assertTrue(file.findVariable(var1).read().getSize() > 0);
     }
