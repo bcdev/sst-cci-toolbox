886c51cf496f061130d53fe3f61ad6158ad0bcbd
Ralf Quast <ralf.quast@brockmann-consult.de>
2011-03-22 10:33:51 +0100

    Improvement

--- src/test/java/org/esa/cci/sst/data/DataFileTest.java
+++ src/test/java/org/esa/cci/sst/data/DataFileTest.java
@@ -1,5 +1,6 @@
 package org.esa.cci.sst.data;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -7,15 +8,21 @@ import static org.junit.Assert.assertNull;
 
 public class DataFileTest {
 
+    private DataFile dataFile;
+
     @Test
     public void testSetGetPath() {
-        final DataFile dataFile = new DataFile();
-
         assertNull(dataFile.getPath());
+
         dataFile.setPath("a\\b");
         assertEquals("a/b", dataFile.getPath());
 
         dataFile.setPath(null);
         assertNull(dataFile.getPath());
+    }
+
+    @Before
+    public void init() {
+        dataFile = new DataFile();
     }
 }
