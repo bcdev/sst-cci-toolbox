2c9d9cdffa7885768252c8ac0bb450eba11f48ec
Ralf Quast <ralf.quast@brockmann-consult.de>
2011-04-12 17:48:41 +0200

    Update

--- src/main/bin/create-mmd-from-scratch.sh
+++ src/main/bin/create-mmd-from-scratch.sh
@@ -1,3 +1,3 @@
-./bin/mmsingest.sh    -c ./config/mmsingest.properties    -debug
-./bin/mmsmatchup.sh   -c ./config/mmsmatchup.properties   -debug
-#./bin/mmscreatemmd.sh -c ./config/mmscreatemmd.properties -debug
+#./bin/mmsingest.sh    -c ./config/mmsingest.properties    -debug
+#./bin/mmsmatchup.sh   -c ./config/mmsmatchup.properties   -debug
+./bin/mmscreatemmd.sh -c ./config/mmscreatemmd.properties -debug
