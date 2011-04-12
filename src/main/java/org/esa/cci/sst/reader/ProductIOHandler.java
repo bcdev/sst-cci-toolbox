e3b870a0cb29df3bbb8b3ab27a1633440f277d35
Ralf Quast <ralf.quast@brockmann-consult.de>
2011-04-12 10:35:25 +0200

    Formatting

--- src/main/java/org/esa/cci/sst/reader/ProductIOHandler.java
+++ src/main/java/org/esa/cci/sst/reader/ProductIOHandler.java
@@ -292,17 +292,17 @@ public class ProductIOHandler implements IOHandler {
 
     private static Number getSample(Raster raster, int x, int y) {
         switch (raster.getTransferType()) {
-        case DataBuffer.TYPE_BYTE:
-        case DataBuffer.TYPE_SHORT:
-        case DataBuffer.TYPE_USHORT:
-        case DataBuffer.TYPE_INT:
-            return raster.getSample(x, y, 0);
-        case DataBuffer.TYPE_FLOAT:
-            return raster.getSampleFloat(x, y, 0);
-        case DataBuffer.TYPE_DOUBLE:
-            return raster.getSampleDouble(x, y, 0);
-        default:
-            throw new IllegalArgumentException("Unsupported transfer type " + raster.getTransferType() + ".");
+            case DataBuffer.TYPE_BYTE:
+            case DataBuffer.TYPE_SHORT:
+            case DataBuffer.TYPE_USHORT:
+            case DataBuffer.TYPE_INT:
+                return raster.getSample(x, y, 0);
+            case DataBuffer.TYPE_FLOAT:
+                return raster.getSampleFloat(x, y, 0);
+            case DataBuffer.TYPE_DOUBLE:
+                return raster.getSampleDouble(x, y, 0);
+            default:
+                throw new IllegalArgumentException("Unsupported transfer type " + raster.getTransferType() + ".");
         }
     }
 
