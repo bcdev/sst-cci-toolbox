bb275d50049b1623c5be442ddc02872c621a3fd6
martin-boettcher <martin.boettcher@brockmann-consult.de>
2011-04-20 14:35:18 +0200

    tool class had been renamed. Script updated

---
+++ src/main/bin/mmsarc12calls.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+if [ ! -d "$CCI_SST_HOME" ]
+then
+    PRGDIR=`dirname $0`
+    export CCI_SST_HOME=`cd "$PRGDIR/.." ; pwd`
+fi
+
+if [ -z "$CCI_SST_HOME" ]; then
+    echo
+    echo Error:
+    echo CCI_SST_HOME does not exists in your environment. Please
+    echo set the CCI_SST_HOME variable in your environment to the
+    echo location of your CCI SST installation.
+    echo
+    exit 2
+fi
+
+MMS_OPTIONS=""
+if [ ! -z $MMS_DEBUG ]; then
+    MMS_OPTIONS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8001,server=y,suspend=y"
+fi
+
+java \
+    -Xmx1024M $MMS_OPTIONS \
+    -javaagent:"$CCI_SST_HOME/lib/openjpa-all.jar" \
+    -classpath "$CCI_SST_HOME/lib/*" \
+    org.esa.cci.sst.tools.arcprocessing.Arc1ProcessingTool "$@"
