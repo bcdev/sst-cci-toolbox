ee6087319ea1f485eae9a54d65540f8b40253b5d
thomasstorm <thomas_storm@brockmann-consult.de>
2011-04-28 18:00:07 +0200

    some progress on variable conversion

---
+++ src/main/java/org/esa/cci/sst/rules/AmsreImageDimensions.java
@@ -0,0 +1,20 @@
+package org.esa.cci.sst.rules;
+
+import org.esa.cci.sst.data.VariableDescriptor;
+
+/**
+ * Replaces the second and third dimension with 'amsre.ni' and 'amsre.nj', respectively.
+ */
+public class AmsreImageDimensions extends DescriptorModification {
+
+    @Override
+    public VariableDescriptor apply(VariableDescriptor sourceDescriptor) throws RuleException {
+        final VariableDescriptor targetDescriptor = new VariableDescriptor(sourceDescriptor);
+        final String sourceDimensions = sourceDescriptor.getDimensions();
+        String targetDimensions;
+        targetDimensions = RuleUtil.replaceDimension(sourceDimensions, "amsre.ni", 1);
+        targetDimensions = RuleUtil.replaceDimension(targetDimensions, "amsre.nj", 2);
+        targetDescriptor.setDimensions(targetDimensions);
+        return targetDescriptor;
+    }
+}
