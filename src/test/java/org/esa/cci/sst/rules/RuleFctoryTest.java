8e0acb90a3bfcd8e8758deb13cb83bcd1d14bec7
flar <ralf.quast@brockmann-consult.de>
2011-04-30 13:31:35 +0200

    Update

---
+++ src/test/java/org/esa/cci/sst/rules/RuleFctoryTest.java
@@ -0,0 +1,56 @@
+package org.esa.cci.sst.rules;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class RuleFctoryTest {
+
+    @Test
+    public void testGetSimpleRule() {
+        final Rule rule = RuleFactory.getInstance().getRule("P");
+
+        assertTrue(rule instanceof P);
+    }
+
+    @Test
+    public void testGetSimpleRuleWithImplicitRenaming() {
+        final Rule rule = RuleFactory.getInstance().getRule("P", "R");
+
+        assertTrue(rule instanceof RightAssociativeComposition);
+
+        final RightAssociativeComposition composition = (RightAssociativeComposition) rule;
+
+        assertEquals(2, composition.getRuleCount());
+        assertTrue(composition.getRule(0) instanceof Renaming);
+        assertTrue(composition.getRule(1) instanceof P);
+    }
+
+    @Test
+    public void testGetRuleComposite() {
+        final Rule rule = RuleFactory.getInstance().getRule("Q,P");
+
+        assertTrue(rule instanceof RightAssociativeComposition);
+
+        final RightAssociativeComposition composition = (RightAssociativeComposition) rule;
+
+        assertEquals(2, composition.getRuleCount());
+        assertTrue(composition.getRule(0) instanceof Q);
+        assertTrue(composition.getRule(1) instanceof P);
+    }
+
+    @Test
+    public void testGetRuleCompositeWithImplicitRenaming() {
+        final Rule rule = RuleFactory.getInstance().getRule("Q,P", "R");
+
+        assertTrue(rule instanceof RightAssociativeComposition);
+
+        final RightAssociativeComposition composition = (RightAssociativeComposition) rule;
+
+        assertEquals(2, composition.getRuleCount());
+        assertTrue(composition.getRule(0) instanceof Renaming);
+        assertTrue(composition.getRule(1) instanceof RightAssociativeComposition);
+    }
+
+}
