2a6ec60f2f3c3f646960f287ce303c39817b9b66
flar <ralf.quast@brockmann-consult.de>
2011-04-30 13:32:20 +0200

    Update

--- src/test/java/org/esa/cci/sst/rules/RuleFactoryTest.java
+++ src/test/java/org/esa/cci/sst/rules/RuleFactoryTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class RuleFctoryTest {
+public class RuleFactoryTest {
 
     @Test
     public void testGetSimpleRule() {
