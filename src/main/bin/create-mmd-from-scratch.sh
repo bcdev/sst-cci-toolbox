9554e8d5a578063a07678d4ebcf15a8e922b07fb
flar <ralf.quast@brockmann-consult.de>
2011-05-05 09:48:20 +0200

    New shell script for creating a database on Mac OS X

--- src/main/bin/create-mmd-from-scratch.sh
+++
@@ -1,3 +0,0 @@
-#./bin/mmsingest.sh    -c ./config/mmsingest.properties    -debug
-#./bin/mmsmatchup.sh   -c ./config/mmsmatchup.properties   -debug
-./bin/mmscreatemmd.sh -c ./config/mmscreatemmd.properties -debug
