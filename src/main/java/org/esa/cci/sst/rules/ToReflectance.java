5ed253c9e5546a0daed0ea1d11619d0ff270215e
Ralf Quast <ralf.quast@brockmann-consult.de>
2011-05-16 15:12:28 +0200

    New 'ToReflectance' rule

---
+++ src/main/java/org/esa/cci/sst/rules/ToReflectance.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2011 Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+
+package org.esa.cci.sst.rules;
+
+import org.esa.cci.sst.data.ColumnBuilder;
+import org.esa.cci.sst.data.Item;
+import ucar.ma2.Array;
+import ucar.ma2.DataType;
+
+/**
+ * Reflectance type.
+ *
+ * @author Ralf Quast
+ */
+public class ToReflectance implements Rule {
+
+    private final double targetAddOffset = 0.0;
+    private final double targetScaleFactor = 0.0001;
+
+    @Override
+    public Item apply(Item sourceColumn) throws RuleException {
+        final ColumnBuilder builder = new ColumnBuilder(sourceColumn);
+        builder.addOffset(targetAddOffset);
+        builder.scaleFactor(targetScaleFactor);
+        builder.type(DataType.SHORT);
+        builder.fillValue(Short.MIN_VALUE);
+
+        return builder.build();
+    }
+
+    @Override
+    public Array apply(Array sourceArray, Item sourceColumn) throws RuleException {
+        final Array targetArray = Array.factory(DataType.SHORT, sourceArray.getShape());
+        apply(sourceArray, targetArray,
+              sourceColumn.getScaleFactor(),
+              sourceColumn.getAddOffset(),
+              sourceColumn.getFillValue());
+
+        return targetArray;
+    }
+
+    private void apply(Array sourceArray, Array targetArray,
+                       Number sourceScaleFactor,
+                       Number sourceAddOffset,
+                       Number sourceFillValue) {
+        final double a = getDouble(sourceScaleFactor, 0.0);
+        final double b = getDouble(sourceAddOffset, 0.0);
+        for (int i = 0; i < sourceArray.getSize(); i++) {
+            final double number = sourceArray.getDouble(i);
+            if (isInvalid(number, sourceFillValue)) {
+                targetArray.setShort(i, Short.MIN_VALUE);
+            }
+            targetArray.setShort(i, apply(number, a, b));
+        }
+    }
+
+    private boolean isInvalid(double d, Number fillValue) {
+        return fillValue != null && d == fillValue.doubleValue() || Double.isNaN(d) || Double.isInfinite(d);
+    }
+
+    private short apply(double d, double sourceScaleFactor, double sourceAddOffset) {
+        return (short) (((sourceScaleFactor * d + sourceAddOffset) - targetAddOffset) / targetScaleFactor);
+    }
+
+    private double getDouble(Number number, double defaultValue) {
+        if (number == null) {
+            return defaultValue;
+        }
+        return number.floatValue();
+    }
+}
