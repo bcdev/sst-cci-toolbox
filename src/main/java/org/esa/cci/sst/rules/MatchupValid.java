af29be93cf5e5d03a819a7c808613ecc25648582
thomasstorm <thomas_storm@brockmann-consult.de>
2011-05-31 14:02:46 +0200

    rule for matchup validity

---
+++ src/main/java/org/esa/cci/sst/rules/MatchupValid.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2011 Brockmann Consult GmbH (info@brockmann-consult.de)
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+
+package org.esa.cci.sst.rules;
+
+import org.esa.cci.sst.data.Item;
+import ucar.ma2.Array;
+import ucar.ma2.DataType;
+
+/**
+ * Rule setting the 'valid' flag for a matchup.
+ *
+ * @author Thomas Storm
+ */
+public class MatchupValid extends AbstractMatchupRule {
+
+    @Override
+    public Array apply(Array sourceArray, Item sourceColumn) throws RuleException {
+        final Array array = Array.factory(DataType.BYTE, new int[]{1});
+        final boolean invalid = getContext().getMatchup().isInvalid();
+        array.setByte(0, (byte) (invalid ? 1 : 0));
+        return array;
+    }
+}
