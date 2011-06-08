980f37232ef44fb6cb75a93481125942f2204a26
thomasstorm <thomas_storm@brockmann-consult.de>
2011-06-08 14:54:38 +0200

    added missing class

--- src/test/java/org/esa/cci/sst/rules/SensorNameGenerator.java
+++ src/test/java/org/esa/cci/sst/rules/SensorNameGenerator.java
@@ -25,50 +25,52 @@ import java.io.IOException;
  */
 public class SensorNameGenerator {
 
-    private static final String CONTENT = "/*\n" +
-                                          " * Copyright (C) 2011 Brockmann Consult GmbH (info@brockmann-consult.de)\n" +
-                                          " * \n" +
-                                          " * This program is free software; you can redistribute it and/or modify it\n" +
-                                          " * under the terms of the GNU General Public License as published by the Free\n" +
-                                          " * Software Foundation; either version 3 of the License, or (at your option)\n" +
-                                          " * any later version.\n" +
-                                          " * This program is distributed in the hope that it will be useful, but WITHOUT\n" +
-                                          " * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or\n" +
-                                          " * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for\n" +
-                                          " * more details.\n" +
-                                          " * \n" +
-                                          " * You should have received a copy of the GNU General Public License along\n" +
-                                          " * with this program; if not, see http://www.gnu.org/licenses/\n" +
-                                          " */\n" +
-                                          "\n" +
-                                          "package org.esa.cci.sst.rules;\n" +
-                                          "\n" +
-                                          "/**\n" +
-                                          " * Sets the sensor to '%s'.\n" +
-                                          " *\n" +
-                                          " * @author Thomas Storm\n" +
-                                          " */\n" +
-                                          "@SuppressWarnings({\"ClassTooDeepInInheritanceTree\", \"UnusedDeclaration\"})\n" +
-                                          "class %s extends SensorRule {\n" +
-                                          "\n" +
-                                          "    %s() {\n" +
-                                          "        super(\"%s\");\n" +
-                                          "    }\n" +
-                                          "}";
+    private static final String CONTENT = new StringBuilder()
+            .append("/*\n")
+            .append(" * Copyright (C) 2011 Brockmann Consult GmbH (info@brockmann-consult.de)\n")
+            .append(" * \n")
+            .append(" * This program is free software; you can redistribute it and/or modify it\n")
+            .append(" * under the terms of the GNU General Public License as published by the Free\n")
+            .append(" * Software Foundation; either version 3 of the License, or (at your option)\n")
+            .append(" * any later version.\n")
+            .append(" * This program is distributed in the hope that it will be useful, but WITHOUT\n")
+            .append(" * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or\n")
+            .append(" * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for\n")
+            .append(" * more details.\n")
+            .append(" * \n")
+            .append(" * You should have received a copy of the GNU General Public License along\n")
+            .append(" * with this program; if not, see http://www.gnu.org/licenses/\n")
+            .append(" */\n")
+            .append('\n')
+            .append("package org.esa.cci.sst.rules;\n")
+            .append('\n')
+            .append("/**\n")
+            .append(" * Sets the sensor to '%s'.\n")
+            .append(" *\n")
+            .append(" * @author Thomas Storm\n")
+            .append(" */\n")
+            .append("@SuppressWarnings({\"ClassTooDeepInInheritanceTree\", \"UnusedDeclaration\"})\n")
+            .append("class %s extends SensorRule {\n")
+            .append('\n')
+            .append("    %s() {\n")
+            .append("        super(\"%s\");\n")
+            .append("    }\n")
+            .append('}').toString();
 
     public static void main(String[] args) throws IOException {
-        createClass("SeaiceSensor", "seaice");
-        createClass("AaiSensor", "aai");
-        createClass("TmiSensor", "tmi");
-        createClass("AmsreSensor", "amsre");
-        createClass("AvhrrTnSensor", "avhrr.tn");
-        createClass("AvhrrM2Sensor", "avhrr.M2");
-        for (int i = 6; i <= 19; i++) {
-            createClass("avhrr." + i, "Avhrr" + i + "Sensor");
-        }
-        for (int i = 1; i <= 3; i++) {
-            createClass("atsr." + i, "Atsr" + i + "Sensor");
-        }
+        createClass("InsituSensor", "history");
+//        createClass("SeaiceSensor", "seaice");
+//        createClass("AaiSensor", "aai");
+//        createClass("TmiSensor", "tmi");
+//        createClass("AmsreSensor", "amsre");
+//        createClass("AvhrrTnSensor", "avhrr.tn");
+//        createClass("AvhrrM2Sensor", "avhrr.M2");
+//        for (int i = 6; i <= 19; i++) {
+//            createClass("avhrr." + i, "Avhrr" + i + "Sensor");
+//        }
+//        for (int i = 1; i <= 3; i++) {
+//            createClass("atsr." + i, "Atsr" + i + "Sensor");
+//        }
     }
 
     private static void createClass(String className, String sensorName) throws IOException {
