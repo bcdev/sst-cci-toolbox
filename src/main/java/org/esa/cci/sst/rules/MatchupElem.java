4879ba99ee2b1c5e1a96e90e9a3e3c29153a9639
thomasstorm <thomas_storm@brockmann-consult.de>
2011-07-18 14:55:37 +0200

    fix for negative values in matchup.line

--- src/main/java/org/esa/cci/sst/rules/MatchupElem.java
+++ src/main/java/org/esa/cci/sst/rules/MatchupElem.java
@@ -16,18 +16,63 @@
 
 package org.esa.cci.sst.rules;
 
+import org.esa.beam.framework.datamodel.GeoCoding;
+import org.esa.beam.framework.datamodel.GeoPos;
 import org.esa.beam.framework.datamodel.PixelPos;
+import org.esa.cci.sst.data.ColumnBuilder;
+import org.esa.cci.sst.data.Item;
+import org.esa.cci.sst.data.ReferenceObservation;
+import org.esa.cci.sst.reader.Reader;
+import org.postgis.Point;
+import ucar.ma2.Array;
+import ucar.ma2.DataType;
+
+import java.io.IOException;
 
 /**
- * Returns the matchup element.
+ * Sets the matchup element.
  *
  * @author Thomas Storm
  */
 @SuppressWarnings({"UnusedDeclaration"})
-class MatchupElem extends MatchupPosition {
+class MatchupElem extends Rule {
+
+    private static final short FILL_VALUE = -1;
 
     @Override
-    protected short getDimension(PixelPos pixelPos) {
+    public Item apply(Item sourceColumn) throws RuleException {
+        return new ColumnBuilder(sourceColumn)
+                .type(DataType.SHORT)
+                .fillValue(FILL_VALUE)
+                .build();
+    }
+
+    @Override
+    public Array apply(Array sourceArray, Item sourceColumn) throws RuleException {
+        final Array array = Array.factory(DataType.SHORT, new int[]{1});
+        array.setShort(0, getValue());
+        return array;
+    }
+
+    private short getValue() throws RuleException {
+        final Context context = getContext();
+        final ReferenceObservation refObs = context.getMatchup().getRefObs();
+        final Point point = refObs.getPoint().getGeometry().getFirstPoint();
+        final double lon = point.getX();
+        final double lat = point.getY();
+        final Reader observationReader = context.getObservationReader();
+        if(observationReader == null) {
+            return FILL_VALUE;
+        }
+        final GeoCoding geoCoding;
+        try {
+            geoCoding = observationReader.getGeoCoding(refObs.getRecordNo());
+        } catch (IOException e) {
+            throw new RuleException("Unable to obtain geo-coding.", e);
+        }
+        final PixelPos pixelPos = geoCoding.getPixelPos(new GeoPos((float) lat, (float) lon), null);
+        pixelPos.x += observationReader.getLineSkip();
         return (short) pixelPos.getX();
     }
+
 }
--- src/main/java/org/esa/cci/sst/rules/MatchupLine.java
+++ src/main/java/org/esa/cci/sst/rules/MatchupLine.java
@@ -16,18 +16,62 @@
 
 package org.esa.cci.sst.rules;
 
+import org.esa.beam.framework.datamodel.GeoCoding;
+import org.esa.beam.framework.datamodel.GeoPos;
 import org.esa.beam.framework.datamodel.PixelPos;
+import org.esa.cci.sst.data.ColumnBuilder;
+import org.esa.cci.sst.data.Item;
+import org.esa.cci.sst.data.ReferenceObservation;
+import org.esa.cci.sst.reader.Reader;
+import org.postgis.Point;
+import ucar.ma2.Array;
+import ucar.ma2.DataType;
+
+import java.io.IOException;
 
 /**
- * Returns the matchup line.
+ * Sets the matchup line.
  *
  * @author Thomas Storm
  */
 @SuppressWarnings({"UnusedDeclaration"})
-class MatchupLine extends MatchupPosition {
+class MatchupLine extends Rule {
+
+   private static final short FILL_VALUE = -1;
 
     @Override
-    protected short getDimension(PixelPos pixelPos) {
-        return (short) pixelPos.getY();
+    public Item apply(Item sourceColumn) throws RuleException {
+        return new ColumnBuilder(sourceColumn)
+                .type(DataType.INT)
+                .fillValue(FILL_VALUE)
+                .build();
+    }
+
+    @Override
+    public Array apply(Array sourceArray, Item sourceColumn) throws RuleException {
+        final Array array = Array.factory(DataType.INT, new int[]{1});
+        final int value = getValue();
+        array.setInt(0, value);
+        return array;
+    }
+
+    private int getValue() throws RuleException {
+        final Context context = getContext();
+        final ReferenceObservation refObs = context.getMatchup().getRefObs();
+        final Point point = refObs.getPoint().getGeometry().getFirstPoint();
+        final double lon = point.getX();
+        final double lat = point.getY();
+        final Reader observationReader = context.getObservationReader();
+        if(observationReader == null) {
+            return FILL_VALUE;
+        }
+        final GeoCoding geoCoding;
+        try {
+            geoCoding = observationReader.getGeoCoding(refObs.getRecordNo());
+        } catch (IOException e) {
+            throw new RuleException("Unable to obtain geo-coding.", e);
+        }
+        final PixelPos pixelPos = geoCoding.getPixelPos(new GeoPos((float) lat, (float) lon), null);
+        return (int) pixelPos.getY();
     }
 }
