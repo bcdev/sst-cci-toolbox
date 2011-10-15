90c76821982394d30bb93863701b403bad798c3d
Norman <norman.fomferra@brockmann-consult.de>
2011-10-15 09:16:44 +0200

    About to generalise various input file types

--- tools/src/test/java/org/esa/cci/sst/regavg/regavg/ProductTypeTest.java
+++ tools/src/test/java/org/esa/cci/sst/regavg/regavg/ProductTypeTest.java
@@ -15,10 +15,10 @@ public class ProductTypeTest {
     @Test
     public void test_ARC_L3U() throws Exception {
         DateFormat format = UTC.getDateFormat("yyyy-MM-dd");
-        Assert.assertEquals(format.parse("2002-01-12"), ProductType.ARC_L3U.getDate(new File("AT2_AVG_3PAARC20020112_D_dN2b.nc.gz")));  // day/nadir
-        Assert.assertEquals(format.parse("2002-04-16"), ProductType.ARC_L3U.getDate(new File("AT2_AVG_3PAARC20020416_D_dN2b.nc.gz")));  // day/dual
-        Assert.assertEquals(format.parse("2002-01-20"), ProductType.ARC_L3U.getDate(new File("AT2_AVG_3PAARC20020120_D_nN2b.nc.gz")));  // night/nadir
-        Assert.assertEquals(format.parse("2002-09-15"), ProductType.ARC_L3U.getDate(new File("ATS_AVG_3PAARC20020915_D_nD3b.nc.gz")));  // night/dual
+        Assert.assertEquals(format.parse("2002-01-12"), ProductType.ARC_L3U.parseDate(new File("AT2_AVG_3PAARC20020112_D_dN2b.nc.gz")));  // day/nadir
+        Assert.assertEquals(format.parse("2002-04-16"), ProductType.ARC_L3U.parseDate(new File("AT2_AVG_3PAARC20020416_D_dN2b.nc.gz")));  // day/dual
+        Assert.assertEquals(format.parse("2002-01-20"), ProductType.ARC_L3U.parseDate(new File("AT2_AVG_3PAARC20020120_D_nN2b.nc.gz")));  // night/nadir
+        Assert.assertEquals(format.parse("2002-09-15"), ProductType.ARC_L3U.parseDate(new File("ATS_AVG_3PAARC20020915_D_nD3b.nc.gz")));  // night/dual
 
     }
 }
