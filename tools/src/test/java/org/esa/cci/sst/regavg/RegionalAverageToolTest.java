6b02b9e94ef97c6eb1e33191c8bb6902cf3aaa90
Norman <norman.fomferra@brockmann-consult.de>
2011-10-24 21:07:37 +0200

    Changed logging time and time in text filename to local time

--- tools/src/test/java/org/esa/cci/sst/regavg/RegionalAverageToolTest.java
+++ tools/src/test/java/org/esa/cci/sst/regavg/RegionalAverageToolTest.java
@@ -12,7 +12,7 @@ public class RegionalAverageToolTest {
     @Test
     public void testOutputName() throws Exception {
         String filename = RegionalAverageTool.getOutputFilename("20000101", "20101231", "Global", ProcessingLevel.L3U, "SSTskin", "PS", "DM");
-        assertEquals("20000101-20101231-Global_average-ESACCI-L3U_GHRSST-SSTskin-PS-DM-v1.0_b01-fv1.1.nc", filename);
+        assertEquals("20000101-20101231-Global_average-ESACCI-L3U_GHRSST-SSTskin-PS-DM-v1.0_b02-fv1.1.nc", filename);
     }
 
 }
