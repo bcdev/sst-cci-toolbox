cf42b5704238ccd155afa3c0b9fe946b67504bb1
Bettina Scholze <bettina.scholze@brockmann-consult.de>
2012-11-19 13:18:48 +0100

    interpolate LUT (LutForXTime) to 0.05 degree

--- tools/src/main/java/org/esa/cci/sst/common/auxiliary/LutForXTime.java
+++ tools/src/main/java/org/esa/cci/sst/common/auxiliary/LutForXTime.java
@@ -1,5 +1,6 @@
 package org.esa.cci.sst.common.auxiliary;
 
+import org.esa.beam.util.math.MathUtils;
 import org.esa.cci.sst.common.cellgrid.ArrayGrid;
 import org.esa.cci.sst.common.cellgrid.Grid;
 import org.esa.cci.sst.common.cellgrid.GridDef;
@@ -10,6 +11,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+import static java.lang.Math.floor;
+
 /**
  * {@author Bettina Scholze}
  * Date: 09.11.12 15:32
@@ -17,6 +20,8 @@ import java.io.IOException;
 public class LutForXTime {
 
     private static final GridDef sourceGridDef = GridDef.createGlobal(2.0);
+    private static final int LENGTH_OF_A_LUT_ENTRY = 9;
+
     private SpatialResolution targetResolution;
     private final double fillValue;
     private Grid lutGrid;
@@ -28,6 +33,19 @@ public class LutForXTime {
     }
 
     public static LutForXTime read(File file, SpatialResolution targetResolution, double fillValue) throws IOException {
+        ArrayGrid lut2Degree = readInAndFlip(file, targetResolution, fillValue);
+
+        //todo interpolate to 0.05°
+        ArrayGrid lut005Degree = interpolateTo005(lut2Degree);
+        //todo scale down to targetResolution
+
+
+//        LutForXTime lutForXTime = new LutForXTime(lutGridTargetResolution, targetResolution, fillValue)
+//        return lutForXTime;
+        return null;
+    }
+
+    static ArrayGrid readInAndFlip(File file, SpatialResolution targetResolution, double fillValue) throws IOException {
         int width = sourceGridDef.getWidth();
         int height = sourceGridDef.getHeight();
         double[] data = new double[width * height];
@@ -38,8 +56,13 @@ public class LutForXTime {
             for (int y = 0; y < height; y++) {
                 String line = bufferedReader.readLine();
                 for (int x = 0; x < width; x++) {
-                    String substring = line.substring(x * 9, x * 9 + 9);
-                    data[width * y + x] = Double.parseDouble(substring);
+                    String substring = line.substring(x * LENGTH_OF_A_LUT_ENTRY, x * LENGTH_OF_A_LUT_ENTRY + LENGTH_OF_A_LUT_ENTRY);
+                    double value = Double.parseDouble(substring);
+                    if (value == -32768.0) {
+                        value = Double.NaN;
+                    }
+                    int flippedY = height - y - 1; //origin of lut is left-down, we need upper-left corner
+                    data[width * flippedY + x] = value;
                 }
             }
         } finally {
@@ -52,22 +75,74 @@ public class LutForXTime {
             }
         }
 
-        ArrayGrid lutGrid = ArrayGrid.createWith2DDoubleArray(sourceGridDef, data);
-        return new LutForXTime(lutGrid, targetResolution, fillValue);
+        return ArrayGrid.createWith2DDoubleArray(sourceGridDef, data);
     }
 
+    /**
+     * Get X-time in target resolution
+     *
+     * @param cellX
+     * @param cellY
+     * @return
+     */
     public double getXTime(int cellX, int cellY) {
-       return this.lutGrid.getSampleDouble(cellX,cellY);
+        return this.lutGrid.getSampleDouble(cellX, cellY);
     }
 
-//    private Grid read() {
-//
-//    }
-//
-//    private Grid scaleTo005() {
-//
-//    }
-//
+    static ArrayGrid interpolateTo005(ArrayGrid lut) {
+        ArrayGrid arrayGrid = ArrayGrid.createWith2DDoubleArray(GridDef.createGlobal(0.05));
+
+        int sourceHeight = lut.getHeight();
+        int sourceWidth = lut.getWidth();
+        int height = arrayGrid.getHeight();
+        int width = arrayGrid.getWidth();
+        int resolutionX = width / sourceWidth;
+        int resolutionY = height / sourceHeight;
+
+        for (int h = 0; h < height; h++) { //finer (target)
+            for (int w = 0; w < width; w++) {
+                double sourceIndexWExact = w * 1.0 / resolutionX;
+                int sourceIndexWLeft = calculateFirstSourceIndex(sourceWidth, resolutionX, w, sourceIndexWExact);
+                double sourceIndexHExact = h * 1.0 / resolutionY;
+                int sourceIndexHUp = calculateFirstSourceIndex(sourceHeight, resolutionY, h, sourceIndexHExact);
+//                System.out.print("sourceIndexWLeft = " + sourceIndexWLeft);
+//                System.out.println("    sourceIndexWExact = " + sourceIndexWExact + "  w=" + w);
+//                System.out.print("sourceIndexHUp = " + sourceIndexHUp);
+//                System.out.println("    sourceIndexHExact = " + sourceIndexHExact + " h=" + h);
+
+                //calculate weight between the corners
+                double ww = -MathUtils.round(sourceIndexWLeft + 0.5 - sourceIndexWExact, 1000);
+                double wh = -MathUtils.round(sourceIndexHUp + 0.5 - sourceIndexHExact, 1000);
+//                System.out.print("ww = " + ww);
+//                System.out.println("    wh = " + wh);
+
+                //get corners' values
+                double v00 = lut.getSampleDouble(sourceIndexWLeft, sourceIndexHUp);
+                double v10 = lut.getSampleDouble(sourceIndexWLeft + 1, sourceIndexHUp);
+                double v01 = lut.getSampleDouble(sourceIndexWLeft, sourceIndexHUp + 1);
+                double v11 = lut.getSampleDouble(sourceIndexWLeft + 1, sourceIndexHUp + 1);
+                //interpolate
+                double value = MathUtils.interpolate2D(ww, wh, v00, v01, v10, v11);
+                arrayGrid.setSample(w, h, value);
+            }
+        }
+        return arrayGrid;
+    }
+
+    private static int calculateFirstSourceIndex(int grossGridExtend, int resolution, int indexInFinerGrid, double exactIndexInGrosserGrid) {
+        int indexInGrosserGridFloored;
+        if (exactIndexInGrosserGrid <= 0.5) {
+            indexInGrosserGridFloored = 0; //boundary
+        } else if (exactIndexInGrosserGrid >= grossGridExtend - 1) {
+            indexInGrosserGridFloored = grossGridExtend - 2; //boundary
+        } else {
+            int halfBox = resolution / 2;
+            indexInGrosserGridFloored = (int) floor((indexInFinerGrid - halfBox) / resolution); //shifted because value is in the middle of the grid box
+//            indexInGrosserGridFloored = (int) floor((indexInFinerGrid - 0.5) / resolution); //shifted because value is in the middle of the grid box
+        }
+        return indexInGrosserGridFloored;
+    }
+
 //    private Grid scaleToTargetResolution() {
 //
 //    }
--- tools/src/test/java/org/esa/cci/sst/common/auxiliary/LutForXTimeTest.java
+++ tools/src/test/java/org/esa/cci/sst/common/auxiliary/LutForXTimeTest.java
@@ -1,5 +1,6 @@
 package org.esa.cci.sst.common.auxiliary;
 
+import org.esa.cci.sst.common.cellgrid.ArrayGrid;
 import org.esa.cci.sst.regrid.SpatialResolution;
 import org.junit.Test;
 
@@ -7,6 +8,7 @@ import java.io.File;
 
 import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * {@author Bettina Scholze}
@@ -15,13 +17,57 @@ import static org.junit.Assert.assertTrue;
 public class LutForXTimeTest {
 
     @Test
-    public void testLutSameResolution() throws Exception {
-        File file = new File("./src/main/conf/auxdata/x0_time.txt");
-        LutForXTime lutForXTime = LutForXTime.read(file, SpatialResolution.DEGREE_2_00, -32768.0);
+    public void testReadInAndFlip() throws Exception {
+        File file;
+        try {
+            file = new File("./src/main/conf/auxdata/x0_time.txt");
+        } catch (Exception e) {
+            throw new Exception("Get x0_time.txt from <fs1:/projects/ongoing/SST-CCI/docs/technical-specification/tools> and put it in <./src/main/conf/auxdata/x0_time.txt>");
+        }
+        ArrayGrid lut = LutForXTime.readInAndFlip(file, SpatialResolution.DEGREE_2_00, -32768.0);
 
-        assertEquals(4.99, lutForXTime.getXTime(0, 0));
-        assertTrue(Double.isNaN(lutForXTime.getXTime(1, 0)));
+        assertEquals(4.99, lut.getSampleDouble(0, 0));
+        assertTrue(Double.isNaN(lut.getSampleDouble(1, 0)));
+        assertEquals(4.11, lut.getSampleDouble(0, 12));
+        assertEquals(3.72, lut.getSampleDouble(1, 13));
+        assertEquals(2.81, lut.getSampleDouble(5, 24));
     }
 
+    @Test
+    public void testInterpolateTo005Degree() throws Exception {
+        File file;
+        try {
+            file = new File("./src/main/conf/auxdata/x0_time.txt");
+        } catch (Exception e) {
+            throw new Exception("Get x0_time.txt from <fs1:/projects/ongoing/SST-CCI/docs/technical-specification/tools> and put it in <./src/main/conf/auxdata/x0_time.txt>");
+        }
+        ArrayGrid lut = LutForXTime.readInAndFlip(file, SpatialResolution.DEGREE_2_00, -32768.0);
 
+        //execution
+        ArrayGrid lut005 = LutForXTime.interpolateTo005(lut);
+
+        assertEquals(3600, lut005.getHeight());
+        assertEquals(7200, lut005.getWidth());
+        assertEquals(1.0, lut005.getScaling());
+        assertEquals(0.0, lut005.getOffset());
+
+        assertEquals(Double.NaN, lut005.getSampleDouble(0, 0));
+        assertEquals(Double.NaN, lut005.getSampleDouble(0, 3559));
+        assertEquals(4.99, lut005.getSampleDouble(1, 140));
+
+        try {
+            lut005.getSampleDouble(0, 3600);
+            fail("ArrayIndexOutOfBoundsException expected");
+        } catch (ArrayIndexOutOfBoundsException expected) {
+        }
+
+        assertEquals(Double.NaN, lut005.getSampleDouble(7199, 3559));
+        assertEquals(2.00478125, lut005.getSampleDouble(7019, 2819));
+        assertEquals(2.18213125, lut005.getSampleDouble(7019, 2719));
+        try {
+            lut005.getSampleDouble(7200, 3559);
+            fail("ArrayIndexOutOfBoundsException expected");
+        } catch (Exception e) {
+        }
+    }
 }
