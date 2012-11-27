98c652268cbaa264f023bb0d6c4aebf2a3e64674
Ralf Quast <ralf.quast@brockmann-consult.de>
2012-11-27 13:27:12 +0100

    Changed file path

--- tools/src/main/java/org/esa/cci/sst/example/L2PGenerator.java
+++ tools/src/main/java/org/esa/cci/sst/example/L2PGenerator.java
@@ -15,7 +15,7 @@ class L2PGenerator {
 
         final ExampleDataGenerator exampleDataGenerator = new ExampleDataGenerator();
         exampleDataGenerator.setGeneratorExecutablePath("/usr/local/bin/ncgen");
-        exampleDataGenerator.setSourceCdlFilePath("src/main/cdl/20100701000000-ESACCI-L2P_GHRSST-SSTskin-AATSR-DM-v02.0-fv01.0.cdl");
+        exampleDataGenerator.setSourceCdlFilePath("/Users/ralf/Public/developer/sst-cci-toolbox/tools/src/main/cdl/20100701000000-ESACCI-L2P_GHRSST-SSTskin-AATSR-DM-v02.0-fv01.0.cdl");
         exampleDataGenerator.setTargetCdlFilePath("20100701000000-ESACCI-L2P_GHRSST-SSTskin-AATSR-DM-v02.0-fv01.0.cdl");
         exampleDataGenerator.getProperties().setProperty("LAT", g.getLatFilePath());
         exampleDataGenerator.getProperties().setProperty("LON", g.getLonFilePath());
