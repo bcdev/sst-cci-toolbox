04699fb8084c354abcaa1eab7b687fc30cf54697
martin-boettcher <martin.boettcher@brockmann-consult.de>
2014-02-13 13:46:38 +0100

    some todos solved in mms.py control script

--- mms/src/main/bin/ingestion-run2.sh
+++ mms/src/main/bin/ingestion-run2.sh
@@ -5,7 +5,6 @@ month=$2
 usecase=$3
 
 ingestion-tool.sh -c $usecase-config.properties \
--Dmms.archive.rootdir=$MMS_ARCHIVE \
 -Dmms.source.11.inputDirectory=atsr.1/v2.1/$year/$month \
 -Dmms.source.12.inputDirectory=atsr.2/v2.1/$year/$month \
 -Dmms.source.13.inputDirectory=atsr.3/v2.1/$year/$month \
