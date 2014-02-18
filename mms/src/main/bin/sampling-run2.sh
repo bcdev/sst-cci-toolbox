784b667584693242c9ffb4157f1dad4b316cfe71
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-02-18 18:12:27 +0100

    Added usecase and skip as parameters

--- mms/src/main/bin/sampling-run2.sh
+++ mms/src/main/bin/sampling-run2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# sampling-run2.sh 2003 01 atsr.3 300000 mms2
+# sampling-run2.sh 2003 01 atsr.3 300000 0 mms2
 # mms/archive/atsr.3/v2.1/2003/01/17/ATS_TOA_1P...N1
 # mms/archive/mms2/smp/atsr.3/2003/atsr.3-smp-2003-01-b.txt
 
@@ -7,7 +7,8 @@ year=$1
 month=$2
 sensor=$3
 count=$4
-usecase=$5
+skip=$5
+usecase=$6
 
 d=`date +%s -u -d "$year-$month-01 00:00:00"`
 let d1="d + 32 * 86400"
@@ -15,8 +16,10 @@ starttime=$year-$month-01T00:00:00Z
 stoptime=`date +%Y-%m -u -d @$d1`-01T00:00:00Z
 
 sampling-tool.sh -c $usecase-config.xml \
+-Dmms.usecase=${usecase} \
 -Dmms.sampling.startTime=${starttime} \
 -Dmms.sampling.stopTime=${stoptime} \
 -Dmms.sampling.sensor=${sensor} \
 -Dmms.sampling.count=${count} \
+-Dmms.sampling.skip=${skip} \
 -Dmms.sampling.cleanupinterval=true
