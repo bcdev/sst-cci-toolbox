1a2fff052b5175786c5a1543851ba1446709ce69
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-02-18 17:04:54 +0100

    more descriptive job name

--- mms/src/main/bin/ingestion-start.sh
+++ mms/src/main/bin/ingestion-start.sh
@@ -12,7 +12,7 @@ echo "`date -u +%Y%m%d-%H%M%S` submitting tasks to ingest $year/$month"
 read_task_jobs ingestion
 
 if [ -z $jobs ]; then
-    line=`ssh lotus.jc.rl.ac.uk bsub -q lotus -n 1 -W 02:00 -P esacci_sst -cwd $MMS_INST -oo $MMS_LOG/ingestion-$year-$month.out -eo $MMS_LOG/ingestion-$year-$month.err -J ing-$year$month $MMS_HOME/bin/ingestion-run2.sh $year $month $usecase`
+    line=`ssh lotus.jc.rl.ac.uk bsub -q lotus -n 1 -W 02:00 -P esacci_sst -cwd $MMS_INST -oo $MMS_LOG/ingestion-$year-$month.out -eo $MMS_LOG/ingestion-$year-$month.err -J ingestion-$year$month $MMS_HOME/bin/ingestion-run2.sh $year $month $usecase`
     echo $line
     jobs=`echo $line | awk '{ print substr($2,2,length($2)-2) }'`
     echo "$MMS_LOG/ingestion-$year-$month.out/$jobs" > $MMS_TASKS/ingestion-$year-$month.tasks
