0bb95381e5a29960b400be51535912868e4980b6
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-02-19 11:36:02 +0100

    fixed assembly

--- mms/src/main/bin/mapplot-tool.sh
+++ mms/src/main/bin/mapplot-tool.sh
@@ -6,7 +6,7 @@
 java \
     -Dmms.home="$MMS_HOME" \
     -Xmx1024M $MMS_OPTIONS \
-    -javaagent:"$MMS_HOME/lib/openjpa-all-2.2.2.jar" \
+    -javaagent:"$MMS_HOME/lib/openjpa-all-${openjpaversion}.jar" \
     -Djava.io.tmpdir=$TMPDIR \
     -classpath "$MMS_HOME/lib/*" \
     org.esa.cci.sst.tools.MapPlotTool "$@"
--- mms/src/main/bin/sampling-tool.sh
+++ mms/src/main/bin/sampling-tool.sh
@@ -6,7 +6,7 @@
 java \
     -Dmms.home="$MMS_HOME" \
     -Xmx3G $MMS_OPTIONS \
-    -javaagent:"$MMS_HOME/lib/openjpa-all-2.2.2.jar" \
+    -javaagent:"$MMS_HOME/lib/openjpa-all-${openjpaversion}.jar" \
     -Djava.io.tmpdir=$TMPDIR \
     -classpath "$MMS_HOME/lib/*" \
     org.esa.cci.sst.tools.SamplingPointGenerator "$@"
