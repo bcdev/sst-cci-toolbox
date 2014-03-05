de1608765e97c5b55c56c2e7f47110186c8bfd91
tom <tom.block@brockmann-consult.de>
2014-03-05 17:48:03 +0100

    added checking for missing properties

--- mms/src/main/java/org/esa/cci/sst/util/ConfigUtil.java
+++ mms/src/main/java/org/esa/cci/sst/util/ConfigUtil.java
@@ -1,9 +1,7 @@
 package org.esa.cci.sst.util;
 
 
-import org.apache.commons.lang.StringUtils;
 import org.esa.cci.sst.tools.Configuration;
-import org.esa.cci.sst.tools.ToolException;
 
 import java.io.File;
 
@@ -11,12 +9,11 @@ public class ConfigUtil {
 
     public static String getUsecaseRootPath(Configuration config) {
         final String archiveRootPath = config.getStringValue(Configuration.KEY_ARCHIVE_ROOTDIR);
-        if (StringUtils.isEmpty(archiveRootPath)) {
-            throw new ToolException("Missing archive root in config.", -1);
-        }
 
-        final String usecase = config.getStringValue(Configuration.KEY_USECASE);
-        if (StringUtils.isEmpty(usecase)) {
+        final String usecase;
+        try {
+            usecase = config.getStringValue(Configuration.KEY_USECASE);
+        } catch (Exception e) {
             return archiveRootPath;
         }
 
