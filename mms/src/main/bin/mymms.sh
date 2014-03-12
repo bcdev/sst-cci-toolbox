920db14d880b1ec8c7bec80007f656e1c2f3dad5
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-03-12 14:12:35 +0100

    Extracted some properties into profile

--- mms/src/main/bin/mymms.sh
+++ mms/src/main/bin/mymms.sh
@@ -4,19 +4,18 @@
 
 umask 007
 
-export PGPORT=5423
-export PGHOST=mms1.cems.rl.ac.uk
+export PGPORT=${mms.pg.port}
+export PGHOST=${mms.pg.host}
 export PGDATA=/data/mboettcher/mms/db/mmdb
 
 export PATH=/group_workspaces/cems/esacci_sst/mms/software/jdk1.7.0_51/bin:${PATH}
 export PATH=/group_workspaces/cems/esacci_sst/mms/software/bin:${PATH}
 export LD_LIBRARY_PATH=/group_workspaces/cems/esacci_sst/mms/software/lib:/group_workspaces/cems/esacci_sst/software/common/lib:${LD_LIBRARY_PATH}
 
-export MMS_HOME=/group_workspaces/cems/esacci_sst/mms/software/sst-cci-mms-${pom.version}
-export MMS_ARCHIVE=/group_workspaces/cems/esacci_sst/mms/archive
+export MMS_HOME=${mms.home}
+export MMS_ARCHIVE=${mms.archive.rootdir}
 
 export MMS_INST=/group_workspaces/cems/esacci_sst/mms/inst
-export MMS_CONFIG=${MMS_INST}/mms-config.properties
 export MMS_TASKS=${MMS_INST}/tasks
 export MMS_LOG=${MMS_INST}/log
 export TMPDIR=${MMS_INST}/tmp
