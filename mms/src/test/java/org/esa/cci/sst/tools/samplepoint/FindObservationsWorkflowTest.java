209d89f4940fee57fd89ac0b8b34148b59248f36
TomBlock <tom.block@brockmann-consult.de>
2014-04-02 09:43:00 +0200

    intermediate

---
+++ mms/src/test/java/org/esa/cci/sst/tools/samplepoint/FindObservationsWorkflowTest.java
@@ -0,0 +1,14 @@
+package org.esa.cci.sst.tools.samplepoint;
+
+import org.esa.cci.sst.tools.Configuration;
+import org.junit.Test;
+
+public class FindObservationsWorkflowTest {
+
+    @Test
+    public void testCreateFromContext() {
+        final Configuration configuration = new Configuration();
+
+        // @todo 1 tb/tb continue here tb 2014-04-01
+    }
+}
