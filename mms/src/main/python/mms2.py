bc7dcd2602ef5c1759234e198e415162fc775c2d
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-04-02 14:22:18 +0200

    increased concurrency

--- mms/src/main/python/mms2.py
+++ mms/src/main/python/mms2.py
@@ -100,7 +100,7 @@ for (sensor, sensorstart, sensorstop) in sensors:
 
 hosts = [('localhost', 240)]
 types = [('ingestion-start.sh', 120),
-         ('sampling-start.sh', 24),
+         ('sampling-start.sh', 48),
          ('clearsky-start.sh', 120),
          ('mmd-start.sh', 120),
          ('coincidence-start.sh', 48),
