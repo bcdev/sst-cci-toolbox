2c3cc445533dfdb569580580fd5a080aaba53778
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-04-03 16:08:18 +0200

    Rule for SST uncertainty

---
+++ mms/src/main/java/org/esa/cci/sst/rules/ToSeaSurfaceTemperatureUncertainty.java
@@ -0,0 +1,25 @@
+package org.esa.cci.sst.rules;
+
+import org.esa.cci.sst.data.ColumnBuilder;
+import org.esa.cci.sst.tools.Constants;
+
+/**
+ * @author Ralf Quast
+ */
+public class ToSeaSurfaceTemperatureUncertainty extends AbstractRescalingToShort {
+
+    ToSeaSurfaceTemperatureUncertainty() {
+        super(0.001, 0.0);
+    }
+
+    @Override
+    protected final void configureTargetColumn(ColumnBuilder targetColumnBuilder) {
+        targetColumnBuilder
+                .unit(Constants.UNIT_SEA_SURFACE_TEMPERATURE)
+                .validMin(-22000.0)
+                .validMax(22000.0)
+                .longName(null);
+    }
+
+
+}
