ede426969c01bb23379624faaaa321a26940d4c8
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-04-29 09:01:24 +0200

    Increased memory for MMD tool

--- mms/src/main/bin/mmd-tool.sh
+++ mms/src/main/bin/mmd-tool.sh
@@ -5,7 +5,7 @@
 
 java \
     -Dmms.home="$MMS_HOME" \
-    -Xmx6G $MMS_OPTIONS \
+    -Xmx8G $MMS_OPTIONS \
     -javaagent:"$MMS_HOME/lib/openjpa-all-${openjpaversion}.jar" \
     -classpath "$MMS_HOME/lib/*" \
     org.esa.cci.sst.tools.mmdgeneration.MmdTool "$@"
--- mms/src/main/bin/mms-env.sh
+++ mms/src/main/bin/mms-env.sh
@@ -82,7 +82,7 @@ wait_for_task_jobs_completion() {
 submit_job() {
     jobname=$1
     command=$2
-    bsubmit="bsub -R rusage[mem=6144] -q lotus -n 1 -W 08:00 -P esacci_sst -cwd ${MMS_INST} -oo ${MMS_LOG}/${jobname}.out -eo ${MMS_LOG}/${jobname}.err -J ${jobname} ${MMS_HOME}/bin/${command} ${@:3}"
+    bsubmit="bsub -R rusage[mem=6144] -q lotus -n 1 -W 12:00 -P esacci_sst -cwd ${MMS_INST} -oo ${MMS_LOG}/${jobname}.out -eo ${MMS_LOG}/${jobname}.err -J ${jobname} ${MMS_HOME}/bin/${command} ${@:3}"
 
     rm -f ${MMS_LOG}/${jobname}.out
     rm -f ${MMS_LOG}/${jobname}.err
