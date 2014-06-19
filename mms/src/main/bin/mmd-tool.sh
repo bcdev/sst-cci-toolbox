6ad319e2cca5b894a46c51e624fa32f9becd0449
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-06-19 08:04:05 +0200

    Updated resource requirements

--- mms/src/main/bin/mmd-tool.sh
+++ mms/src/main/bin/mmd-tool.sh
@@ -5,7 +5,7 @@
 
 java \
     -Dmms.home="$MMS_HOME" \
-    -Xmx12G $MMS_OPTIONS \
+    -Xmx14G $MMS_OPTIONS \
     -javaagent:"$MMS_HOME/lib/openjpa-all-${openjpaversion}.jar" \
     -classpath "$MMS_HOME/lib/*" \
     org.esa.cci.sst.tools.mmdgeneration.MmdTool "$@"
