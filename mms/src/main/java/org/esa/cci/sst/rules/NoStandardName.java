a96de75b8b476b8bb16c947355931e510a0dcc8f
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-06-30 17:48:49 +0200

    Fixed standard name problem in ECMWF CI, ASN and SSTK variables

---
+++ mms/src/main/java/org/esa/cci/sst/rules/NoStandardName.java
@@ -0,0 +1,15 @@
+package org.esa.cci.sst.rules;
+
+import org.esa.cci.sst.data.ColumnBuilder;
+import org.esa.cci.sst.data.Item;
+
+/**
+ * @author Ralf Quast
+ */
+public final class NoStandardName extends AbstractAttributeModification {
+
+    @Override
+    protected void configureTargetColumn(ColumnBuilder targetColumnBuilder, Item sourceColumn) throws RuleException {
+        targetColumnBuilder.standardName(null);
+    }
+}
