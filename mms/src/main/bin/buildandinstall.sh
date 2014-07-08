5c4832af83f6600ae2beb23d711da7d3fb7a6e5b
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-07-08 11:14:12 +0200

    Update

--- mms/src/main/bin/buildandinstall.sh
+++ mms/src/main/bin/buildandinstall.sh
@@ -11,5 +11,8 @@ mvn -f ${mms.github}/pom.xml clean package assembly:assembly
 # change file permissions manually because maven assembly does not do this
 chmod -R ug+X ${mms.github}/target/sst-cci-mms-${project.version}-bin/sst-cci-mms-${project.version}
 
+# remove old software version
+rm -rf ${mms.home}
+
 # move assembly to target directory
-mv -f ${mms.github}/target/sst-cci-mms-${project.version}-bin/sst-cci-mms-${project.version} $(dirname ${mms.home})
+mv ${mms.github}/target/sst-cci-mms-${project.version}-bin/sst-cci-mms-${project.version} $(dirname ${mms.home})
