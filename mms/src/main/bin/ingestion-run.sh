b2785158e2b755cde484afdf7ce968ef9a187f2a
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-07-29 13:11:32 +0200

    Updated paths to AVHRR files

--- mms/src/main/bin/ingestion-run.sh
+++ mms/src/main/bin/ingestion-run.sh
@@ -13,8 +13,8 @@ ingestion-tool.sh -c ${MMS_HOME}/config/${usecase}-config.properties \
 -Dmms.source.11.inputDirectory=atsr.1/v2.1/${year}/${month} \
 -Dmms.source.12.inputDirectory=atsr.2/v2.1/${year}/${month} \
 -Dmms.source.13.inputDirectory=atsr.3/v2.1/${year}/${month} \
--Dmms.source.22.inputDirectory=avhrr.n10/${year}/${month} \
--Dmms.source.23.inputDirectory=avhrr.n11/${year}/${month} \
--Dmms.source.24.inputDirectory=avhrr.n12/${year}/${month} \
+-Dmms.source.22.inputDirectory=avhrr.n10/v01/${year}/${month} \
+-Dmms.source.23.inputDirectory=avhrr.n11/v01/${year}/${month} \
+-Dmms.source.24.inputDirectory=avhrr.n12/v01/${year}/${month} \
 -Dmms.source.43.inputDirectory=aerosol-aai/v01/${year}/${month} \
 -Dmms.source.44.inputDirectory=sea-ice/v01/${year}/${month}
