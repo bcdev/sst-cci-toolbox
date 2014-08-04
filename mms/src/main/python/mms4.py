846f170cc50d2027b3202807140d713457e8b346
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-08-04 11:58:48 +0200

    Next step

--- mms/src/main/python/mms4.py
+++ mms/src/main/python/mms4.py
@@ -182,13 +182,13 @@ for year in years:
                        ['/nwp/' + sensor + '/' + year + '/' + month],
                        ['/arc/' + sensor + '/' + year + '/' + month],
                        parameters=[year, month, sensor, usecase])
-            continue
 
             # 8. Re-ingest sensor sub-scenes into database
             pm.execute('reingestion-run.sh',
                        ['/sub/' + sensor + '/' + year + '/' + month],
                        ['/con/' + sensor + '/' + year + '/' + month],
                        parameters=[year, month, sensor, 'sub', usecase])
+            continue
 
             # 9. Re-ingest sensor NWP data into database
             pm.execute('reingestion-run.sh',
