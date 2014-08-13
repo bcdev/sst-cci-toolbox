f3258d0f33395b02fc52982f529a8b8677114091
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-08-13 18:04:17 +0200

    Added todos

--- mms/src/main/python/mms3.py
+++ mms/src/main/python/mms3.py
@@ -9,4 +9,5 @@ w.add_primary_sensor('atsr.2', '1995-06-01', '2003-06-23')
 w.add_primary_sensor('atsr.3', '2002-05-20', '2012-04-09')
 w.set_samples_per_month(0)
 
+# todo - the sampling tool fails when sampling from in-situ data with higher concurrency
 w.run(mmdtype, calls=[('sampling-start.sh', 1)], with_history=True)
--- mms/src/main/python/mms4.py
+++ mms/src/main/python/mms4.py
@@ -10,4 +10,5 @@ w.add_primary_sensor('avhrr.n11', '1988-11-08', '1995-01-01')
 w.add_primary_sensor('avhrr.n12', '1991-09-16', '1998-12-15')
 w.set_samples_per_month(0)
 
+# todo - the sampling tool fails when sampling from in-situ data with higher concurrency
 w.run(mmdtype, calls=[('sampling-start.sh', 1)], with_history=True)
