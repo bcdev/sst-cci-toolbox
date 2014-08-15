17a888d0d2fb27e4b1396da2432c01e2896fc17e
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-08-15 16:38:51 +0200

    Update

---
+++ mms/src/main/python/mms11a.py
@@ -0,0 +1,16 @@
+from workflow import Period
+from workflow import Workflow
+
+usecase = 'mms11a'
+mmdtype = 'mmd11'
+
+w = Workflow(usecase, Period('1991-01-01', '1992-01-01'))
+w.add_primary_sensor('avhrr.n10', '1986-11-17', '1991-09-17')
+w.add_primary_sensor('avhrr.n11', '1988-11-08', '1995-01-01')
+w.add_primary_sensor('avhrr.n12', '1991-09-16', '1998-12-15')
+w.add_secondary_sensor('avhrr.n10', '1986-11-17', '1991-09-17')
+w.add_secondary_sensor('avhrr.n11', '1988-11-08', '1995-01-01')
+w.add_secondary_sensor('avhrr.n12', '1991-09-16', '1998-12-15')
+w.set_samples_per_month(500000)
+
+w.run(mmdtype)
