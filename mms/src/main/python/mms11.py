aec1a57d573a964389db2dce369c151f63313552
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-08-15 15:13:11 +0200

    Updated dirty mask expressions

--- mms/src/main/python/mms11.py
+++ mms/src/main/python/mms11.py
@@ -12,7 +12,17 @@ w.add_secondary_sensor('avhrr.n10', '1986-11-17', '1991-09-17')
 w.add_secondary_sensor('avhrr.n11', '1988-11-08', '1995-01-01')
 w.add_secondary_sensor('avhrr.n12', '1991-09-16', '1998-12-15')
 w.set_samples_per_month(500000)
+w.run(mmdtype)
 
-hosts = [('localhost', 60)]
+usecase = 'mms11b'
+mmdtype = 'mmd11'
 
-w.run(mmdtype, hosts)
+w = Workflow(usecase, Period('1991-01-01', '1992-01-01'))
+w.add_primary_sensor('avhrr.n10', '1986-11-17', '1991-09-17')
+w.add_primary_sensor('avhrr.n11', '1988-11-08', '1995-01-01')
+w.add_primary_sensor('avhrr.n12', '1991-09-16', '1998-12-15')
+w.add_secondary_sensor('avhrr.n10', '1986-11-17', '1991-09-17')
+w.add_secondary_sensor('avhrr.n11', '1988-11-08', '1995-01-01')
+w.add_secondary_sensor('avhrr.n12', '1991-09-16', '1998-12-15')
+w.set_samples_per_month(50000)
+w.run(mmdtype)
