ab1cf4000d88015cb7b4964913ee3ddedaad5808
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-08-29 14:49:21 +0200

    Update

--- mms/src/main/python/mms10b.py
+++ mms/src/main/python/mms10b.py
@@ -20,7 +20,7 @@ w.add_primary_sensor('atsr.3', '2002-05-20', '2012-04-09')
 w.add_secondary_sensor('avhrr.n10', '1986-11-17', '1991-09-17')
 w.add_secondary_sensor('avhrr.n11', '1988-11-08', '1995-01-01')
 w.add_secondary_sensor('avhrr.n12', '1991-09-16', '1998-12-15')
-w.set_samples_per_month(500000)
+w.set_samples_per_month(50000)
 
 hosts = [('localhost', 60)]
 
