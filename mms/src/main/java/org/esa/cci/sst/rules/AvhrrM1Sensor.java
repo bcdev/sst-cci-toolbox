36d75964af362b2642fd678e2cc3e6cb0e1404bf
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-09-01 15:38:52 +0200

    Rule update (METOP-A/B)

--- mms/src/main/java/org/esa/cci/sst/rules/AvhrrM1Sensor.java
+++ mms/src/main/java/org/esa/cci/sst/rules/AvhrrM1Sensor.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2011 Brockmann Consult GmbH (info@brockmann-consult.de)
- * 
+ * Copyright (C) 2014 Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option)
@@ -9,7 +9,7 @@
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, see http://www.gnu.org/licenses/
  */
@@ -17,13 +17,13 @@
 package org.esa.cci.sst.rules;
 
 /**
- * Sets the sensor to 'avhrr.m02'.
+ * Sets the sensor to 'avhrr.m01'.
  *
- * @author Thomas Storm
+ * @author Ralf Quast
  */
-final class AvhrrM2Sensor extends SensorRule {
+final class AvhrrM1Sensor extends SensorRule {
 
-    AvhrrM2Sensor() {
-        super("avhrr.m02");
+    AvhrrM1Sensor() {
+        super("avhrr.m01");
     }
 }
