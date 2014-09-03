0ee1b4ef9aa7b46820943d00b25c5d3601a8380c
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-09-03 18:41:30 +0200

    Relaxed accuracy constraint

--- mms/src/main/java/org/esa/beam/dataio/cci/sst/NcAvhrrGacProductReader.java
+++ mms/src/main/java/org/esa/beam/dataio/cci/sst/NcAvhrrGacProductReader.java
@@ -86,7 +86,7 @@ public class NcAvhrrGacProductReader extends NetcdfProductReaderTemplate {
     protected void addGeoCoding(Product product) throws IOException {
         final Band latBand = product.getBand("lat");
         final Band lonBand = product.getBand("lon");
-        final PixelLocator pixelLocator = PixelLocatorFactory.forSwath(lonBand, latBand, 0.025);
+        final PixelLocator pixelLocator = PixelLocatorFactory.forSwath(lonBand, latBand, 0.05);
         final GeoCoding geoCoding = new PixelLocatorAdapter(pixelLocator);
 
         product.setGeoCoding(geoCoding);
