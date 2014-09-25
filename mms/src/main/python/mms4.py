4342c035ede42c4aea6f16324505ac97bfb0be26
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-09-25 15:11:08 +0200

    Changed production period

--- mms/src/main/python/mms4.py
+++ mms/src/main/python/mms4.py
@@ -4,7 +4,7 @@ from workflow import Workflow
 usecase = 'mms4'
 mmdtype = 'mmd4'
 
-w = Workflow(usecase, Period('1991-01-01', '1992-01-01'))
+w = Workflow(usecase, Period('2010-09-01', '2010-10-01'))
 w.add_primary_sensor('avhrr.n10', '1986-11-17', '1991-09-17')
 w.add_primary_sensor('avhrr.n11', '1988-11-08', '1995-01-01')
 w.add_primary_sensor('avhrr.n12', '1991-09-16', '1998-12-15')
