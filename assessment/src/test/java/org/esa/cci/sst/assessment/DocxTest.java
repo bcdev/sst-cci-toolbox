fce3a42b65fa48740531ebeed136adc0215a5aea
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-11-18 11:55:15 +0100

    Deleted Python code; made test for using docx4j

---
+++ assessment/src/test/java/org/esa/cci/sst/assessment/DocxTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2014 Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+
+package org.esa.cci.sst.assessment;
+
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+
+import java.io.File;
+
+/**
+ * @author Ralf Quast
+ */
+public class DocxTest {
+
+    @org.junit.Test
+    public void testDocx() throws Exception {
+        final File templateFile = new File("/Users/ralf/Desktop/Trends_and_Variability_Framework.docx");
+        final WordprocessingMLPackage template = WordprocessingMLPackage.load(templateFile);
+
+        template.getMainDocumentPart().addStyledParagraphOfText("Title", "Hello World!");
+        template.getMainDocumentPart().addStyledParagraphOfText("Heading1", "Hello World!");
+        template.getMainDocumentPart().addStyledParagraphOfText("Normal", "Hello World!");
+        template.save(new File("/Users/ralf/Desktop/car.docx"));
+    }
+}
