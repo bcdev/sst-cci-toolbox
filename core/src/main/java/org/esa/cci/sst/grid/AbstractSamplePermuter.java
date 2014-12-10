c9f282b5a925705474bcb5711a746fe45f9dfb73
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-12-10 09:34:26 +0100

    Fixed copyright

--- core/src/main/java/org/esa/cci/sst/grid/AbstractSamplePermuter.java
+++ core/src/main/java/org/esa/cci/sst/grid/AbstractSamplePermuter.java
@@ -14,27 +14,12 @@
  * with this program; if not, see http://www.gnu.org/licenses/
  */
 
-package org.esa.cci.sst.grid;/*
- * Copyright (C) 2012 Brockmann Consult GmbH (info@brockmann-consult.de)
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 3 of the License, or (at your option)
- * any later version.
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, see http://www.gnu.org/licenses/
- */
-
+package org.esa.cci.sst.grid;
 
 /**
- * Grid decorator for permuting sample values. Can be for, e.g., flipping sample
- * values in y-direction or transposing the sample values for the left and right
- * halves of a grid.
+ * Grid decorator for permuting sample values. Is used for flipping sample
+ * values in y-direction or transposing the sample values for the left and
+ * right halves of a grid.
  *
  * @author Ralf Quast
  */
