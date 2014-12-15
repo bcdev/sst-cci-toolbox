fd14432653b0fd57bc0072a57341ae2c59931dbf
Ralf Quast <ralf.quast@brockmann-consult.de>
2014-12-15 15:02:30 +0100

    Test set-up

--- mms/src/main/python/mms11a.py
+++ mms/src/main/python/mms11a.py
@@ -4,7 +4,7 @@ from workflow import Workflow
 usecase = 'mms11a'
 mmdtype = 'mmd11'
 
-w = Workflow(usecase, Period('1991-01-01', '1992-01-01'))
+w = Workflow(usecase, Period('1991-01-01', '1991-02-01'))
 w.add_primary_sensor('avhrr.n10', '1986-11-17', '1991-09-17')
 w.add_primary_sensor('avhrr.n11', '1988-11-08', '1995-01-01')
 w.add_primary_sensor('avhrr.n12', '1991-09-16', '1998-12-15')
