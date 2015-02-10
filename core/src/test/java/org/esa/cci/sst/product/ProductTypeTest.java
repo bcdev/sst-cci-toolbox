3ed2af91e43dbf27ace8df9580da6e2e6673463f
TomBlock <tom.block@brockmann-consult.de>
2015-02-10 17:24:44 +0100

    added tests

--- core/src/test/java/org/esa/cci/sst/product/ProductTypeTest.java
+++ core/src/test/java/org/esa/cci/sst/product/ProductTypeTest.java
@@ -16,8 +16,17 @@
 
 package org.esa.cci.sst.product;
 
+import org.esa.cci.sst.common.ProcessingLevel;
+import org.esa.cci.sst.grid.GridDef;
 import org.junit.Test;
 
+import java.io.File;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
 /**
@@ -32,5 +41,40 @@ public class ProductTypeTest {
         assertSame(CciL3FileType.INSTANCE, ProductType.CCI_L3U.getFileType());
         assertSame(CciL3FileType.INSTANCE, ProductType.CCI_L3C.getFileType());
         assertSame(CciL4FileType.INSTANCE, ProductType.CCI_L4.getFileType());
+    }
+
+    @SuppressWarnings("OctalInteger")
+    @Test
+    public void testParseDate() throws ParseException {
+        final Date date = ProductType.CCI_L4.parseDate(new File("20070622_we_dont_care"));
+
+        final Calendar calendar = GregorianCalendar.getInstance();
+        calendar.setTime(date);
+        assertEquals(2007, calendar.get(Calendar.YEAR));
+        assertEquals(06, calendar.get(Calendar.MONTH) + 1);
+        assertEquals(22, calendar.get(Calendar.DAY_OF_MONTH));
+    }
+
+    @Test
+    public void testGetDefaultFileNameRegexp() {
+        final String regex = ProductType.CCI_L3C.getDefaultFilenameRegex();
+
+        assertEquals("\\d{14}-ESACCI-L3[CU]_GHRSST-SST((skin)|(subskin)|(depth)|(fnd))-((ATSR1)|(ATSR2)|(AATSR)|(AMSRE)|(AVHRR_MTA)|(SEVIRI_SST)|(TMI))-((LT)|(DM))-v\\d{1,2}\\.\\d{1}-fv\\d{1,2}\\.\\d{1}.nc", regex);
+    }
+
+    @Test
+    public void estGetGriDef() {
+        final GridDef gridDef = ProductType.CCI_L3U.getGridDef();
+
+        assertEquals(3600, gridDef.getHeight());
+        assertEquals(7200, gridDef.getWidth());
+        assertEquals(1, gridDef.getTime());
+    }
+
+    @Test
+    public void testGetProcessingLevel() {
+        final ProcessingLevel processingLevel = ProductType.ARC_L3U.getProcessingLevel();
+
+        assertEquals(ProcessingLevel.L3U, processingLevel);
     }
 }
