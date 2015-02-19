a72081fd7f1b3e5194785d3af9cdd7bfcf04c446
Ralf Quast <ralf.quast@brockmann-consult.de>
2015-02-19 10:23:36 +0100

    Modified mms11b workflow

--- mms/src/main/python/mms10b.py
+++ mms/src/main/python/mms10b.py
@@ -7,6 +7,18 @@ from workflow import Workflow
 usecase = 'mms10b'
 mmdtype = 'mmd10'
 
+w.run(mmdtype, hosts=[('localhost', 48)], with_selection=True)
+
+w = Workflow(usecase)
+w.add_primary_sensor('atsr.2', '1996-07-01', '2003-06-23')
+w.add_primary_sensor('atsr.3', '2002-05-20', '2012-04-09')
+w.add_secondary_sensor('avhrr.n17', '2002-07-10', '2003-04-09')
+w.add_secondary_sensor('avhrr.n17', '2003-12-21', '2011-01-01')
+w.add_secondary_sensor('avhrr.m02', '2006-11-21', '2014-01-01')
+w.set_samples_per_month(5000000)
+
+w.run(mmdtype, hosts=[('localhost', 48)], with_selection=True)
+
 w = Workflow(usecase)
 w.add_primary_sensor('atsr.1', '1991-08-01', '1996-09-01')
 w.add_primary_sensor('atsr.1', '1996-10-01', '1996-11-01')
@@ -30,15 +42,3 @@ w.add_secondary_sensor('avhrr.n16', '2003-12-21', '2011-01-01')
 w.add_secondary_sensor('avhrr.n18', '2005-06-05', '2014-01-01')
 w.add_secondary_sensor('avhrr.n19', '2009-02-06', '2014-01-01')
 w.set_samples_per_month(40000000)
-
-w.run(mmdtype, hosts=[('localhost', 48)], with_selection=True)
-
-w = Workflow(usecase)
-w.add_primary_sensor('atsr.2', '1996-07-01', '2003-06-23')
-w.add_primary_sensor('atsr.3', '2002-05-20', '2012-04-09')
-w.add_secondary_sensor('avhrr.n17', '2002-07-10', '2003-04-09')
-w.add_secondary_sensor('avhrr.n17', '2003-12-21', '2011-01-01')
-w.add_secondary_sensor('avhrr.m02', '2006-11-21', '2014-01-01')
-w.set_samples_per_month(5000000)
-
-w.run(mmdtype, hosts=[('localhost', 48)], with_selection=True)
