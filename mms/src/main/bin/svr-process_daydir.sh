2d87b1a3e32969d30fa687956285b386d3747c2b
Ralf Quast <ralf.quast@brockmann-consult.de>
2015-03-06 15:21:33 +0100

    ?

--- mms/src/main/bin/svr-process_daydir.sh
+++ mms/src/main/bin/svr-process_daydir.sh
@@ -8,8 +8,8 @@ TYPE=$3
 mkdir -p ${OUT_DIR}
 
 # loop over files in the directoy..
-for file in $(ls ${DATA_DIR}/*.nc)
+for input_file in $(ls ${DATA_DIR}/*.nc)
 do
     # run python code..
-    ${mms.python.exec} ${mms.home}/python/svr-verify_SST_CCI_data.py ${file} ${TYPE} ${OUT_DIR}
+    ${mms.python.exec} ${mms.home}/python/svr-verify_SST_CCI_data.py ${input_file} ${TYPE} ${OUT_DIR}
 done
