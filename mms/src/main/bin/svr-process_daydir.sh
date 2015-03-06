cd6def6f5e25789bfc8314822030fb8316713cc4
Ralf Quast <ralf.quast@brockmann-consult.de>
2015-03-06 15:35:52 +0100

    Comment

--- mms/src/main/bin/svr-process_daydir.sh
+++ mms/src/main/bin/svr-process_daydir.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-DATA_DIR=$1
-OUT_DIR=$2
+# Use ABSOLUTE directory paths!
+SOURCE_DIR=$1
+TARGET_DIR=$2
 TYPE=$3
 
 # check directory exists..
-mkdir -p ${OUT_DIR}
+mkdir -p ${TARGET_DIR}
 
 # loop over files in the directoy..
-for input_file in $(ls ${DATA_DIR}/*.nc)
+for f in $(ls ${SOURCE_DIR}/*.nc)
 do
-    # run python code..
-    ${mms.python.exec} ${mms.home}/python/svr-verify_SST_CCI_data.py ${input_file} ${TYPE} ${OUT_DIR}
+    ${mms.python.exec} ${mms.home}/python/svr-verify_SST_CCI_data.py ${f} ${TYPE} ${TARGET_DIR}
 done
