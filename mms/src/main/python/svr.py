54266cb006fbd4b2111c72742d7a61b4fd31c46c
tom <tom.block@brockmann-consult.de>
2015-03-10 16:13:14 +0100

    first pmonitor steps ...

--- mms/src/main/python/svr.py
+++ mms/src/main/python/svr.py
@@ -1,10 +1,24 @@
 from svr_workflow import Svr_Workflow
+from pmonitor import PMonitor
 
-
-
-input_dir = "/usr/local/data/SST-CCI/*.nc"
+input_dir = "/usr/local/data/SST-CCI"
 output_dir = "/usr/local/data/delete"
 sensor_name = "AVHRR"
 
-w = Svr_Workflow(input_dir, output_dir, sensor_name)
-w.run()
+class Svr:
+
+    def __init__(self):
+        self.preconditions = list()
+
+        self.pm = PMonitor(self.preconditions, "svr")
+        # enable line below to skip the real processing tb 2015-03-10
+        # self.pm._simulation = True
+
+    def run(self):
+        self.pm.execute("python svr_workflow.py " + input_dir + " " + output_dir + " " + sensor_name, self.preconditions, list())
+        self.pm.wait_for_completion()
+
+if __name__ == "__main__":
+
+    svr = Svr()
+    svr.run()
--- mms/src/main/python/svr_workflow.py
+++ mms/src/main/python/svr_workflow.py
@@ -3,20 +3,31 @@ import svr_verify_SST_CCI_data
 
 class Svr_Workflow:
 
-    def __init__(self, input_dir, output_dir, sensor_name):
+    def __init__(self, input_wildcard, output_dir, sensor_name):
         """
-        :param input_dir: string
+        :param input_wildcard: string
         :param output_dir: string
         :param sensor_name: string
         """
-        self.input_dir = input_dir
+        self.input_wildcard = input_wildcard
         self.output_dir = output_dir
         self.sensor_name = sensor_name
 
     def run(self):
-        file_list = glob.glob(self.input_dir)
-        print("Found " + str(len(file_list)) + " input files in '" + self.input_dir + "'")
+        file_list = glob.glob(self.input_wildcard)
+        print("Found " + str(len(file_list)) + " input files in '" + self.input_wildcard + "'")
 
         for input_file in file_list:
             print(input_file)
             svr_verify_SST_CCI_data.verify_SST_CCI_data(input_file, self.sensor_name, self.output_dir)
+
+if __name__ == "__main__":
+    import sys
+
+    input_wildcard = sys.argv[1]
+    if input_wildcard[-1] == '/':
+        input_wildcard = sys.argv[1][0:-1]
+    input_wildcard += "/*.nc"
+
+    w = Svr_Workflow(input_wildcard, sys.argv[2], sys.argv[3])
+    w.run()
