a5eb2be5a0482ca301beffe538f1d46dcf65e462
Ralf Quast <ralf.quast@brockmann-consult.de>
2015-03-31 18:00:47 +0200

    deleted Claire's shell scripts

--- mms/src/main/bin/svr-run_qf_check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-files=/disk/scratch/local.2/cbulgin/SVR_DV/new/20100310221800-ESACCI-L2P_GHRSST-SSTskin-AVHRR17_G-LT-v02.0-fv01.0.nc
-
-for f in $files
-do 
-    python svr-qf_check.py $f
-done
