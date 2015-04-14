ab9c6fa9cc8fcf7656dc36a2a5bb794f6697c55d
Ralf Quast <ralf.quast@brockmann-consult.de>
2015-04-14 09:49:20 +0200

    Configurations for MMS6

--- mms/src/main/python/mms6b.py
+++ mms/src/main/python/mms6b.py
@@ -4,7 +4,7 @@ usecase = 'mms6b'
 mmdtype = 'mmd6'
 
 w = Workflow(usecase)
-w.add_primary_sensor('amsr2', '2012-07-02', '2015-01-01')
-w.set_samples_per_month(300000)
+w.add_primary_sensor('amsr2', '2012-07-02', '2015-04-01')
+w.set_samples_per_month(3000000)
 
 w.run(mmdtype, hosts=[('localhost', 48)], calls=[('sampling-start', 1)], with_history=True, without_arc=True)
