71131ec862376107d2fc951c4f117511f0bf00ad
Ralf Quast <ralf.quast@brockmann-consult.de>
2015-04-14 10:14:11 +0200

    Changed directory for AMSR2 data

--- mms/src/main/bin/ingestion-run.sh
+++ mms/src/main/bin/ingestion-run.sh
@@ -25,6 +25,6 @@ ${mms.home}/bin/ingestion-tool.sh -c ${mms.home}/config/${usecase}-config.proper
 -Dmms.source.33.inputDirectory=avhrr.m01/v01/${year}/${month} \
 -Dmms.source.34.inputDirectory=avhrr_f.m02/v01/${year}/${month} \
 -Dmms.source.35.inputDirectory=avhrr_f.m01/v01/${year}/${month} \
--Dmms.source.36.inputDirectory=amsr2/v01/${year}/${month} \
+-Dmms.source.36.inputDirectory=amsr2/v1/${year}/${month} \
 -Dmms.source.43.inputDirectory=aerosol-aai/v01/${year}/${month} \
 -Dmms.source.44.inputDirectory=sea-ice/v01/${year}/${month}
