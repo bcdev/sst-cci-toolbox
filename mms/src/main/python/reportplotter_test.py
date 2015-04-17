8542134a2be49c0fd5d08a8fa9306da4a789bef6
Ralf Quast <ralf.quast@brockmann-consult.de>
2015-04-17 11:57:50 +0200

    Test plotting for AATSR

--- mms/src/main/python/reportplotter_test.py
+++ mms/src/main/python/reportplotter_test.py
@@ -7,6 +7,10 @@ from productverifier import ProductVerifier
 
 
 class ReportPlotterTests(unittest.TestCase):
-    def test_plot_report(self):
-        plotter = ReportPlotter('l2p', 'AVHRR12_G', ProductVerifier.load_report('testdata/summary.json'))
+    def test_plot_report_AVHRR(self):
+        plotter = ReportPlotter('l2p', 'AVHRR12_G', ProductVerifier.load_report('testdata/AVHRR-summary.json'))
+        plotter.plot()
+
+    def test_plot_report_AATSR(self):
+        plotter = ReportPlotter('l2p', 'AATSR', ProductVerifier.load_report('testdata/AATSR-summary.json'))
         plotter.plot()
