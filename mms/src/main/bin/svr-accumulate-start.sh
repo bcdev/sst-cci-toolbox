97b6e2c443d1a1d37a0852409cf80dc11f5e54f1
Ralf Quast <ralf.quast@brockmann-consult.de>
2015-04-22 18:06:32 +0200

    Fixed parameters for SVR scripts

--- mms/src/main/bin/svr-accumulate-start.sh
+++ mms/src/main/bin/svr-accumulate-start.sh
@@ -15,7 +15,7 @@ echo "`date -u +%Y%m%d-%H%M%S` submitting job '$jobname'"
 read_task_jobs $jobname
 
 if [ -z $jobs ]; then
-    submit_job $jobname 8192 0:30 $command
+    submit_job 8192 0:30 $jobname $command
 fi
 
 wait_for_task_jobs_completion $jobname
--- mms/src/main/bin/svr-plot-start.sh
+++ mms/src/main/bin/svr-plot-start.sh
@@ -16,7 +16,7 @@ echo "`date -u +%Y%m%d-%H%M%S` submitting job '$jobname'"
 read_task_jobs $jobname
 
 if [ -z $jobs ]; then
-    submit_job $jobname 8192 0:10 $command
+    submit_job 8192 0:10 $jobname $command
 fi
 
 wait_for_task_jobs_completion $jobname
