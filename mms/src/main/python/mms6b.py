963768ece43cdfe968bf6a23ef81c8e39b3b1515
Ralf Quast <ralf.quast@brockmann-consult.de>
2015-05-11 10:00:14 +0200

    Formatting

--- mms/src/main/python/mms6b.py
+++ mms/src/main/python/mms6b.py
@@ -8,4 +8,6 @@ w = Workflow(usecase, Period('2012-07-02', '2015-01-01'))
 w.add_primary_sensor('amsr2', '2012-07-02', '2015-04-01')
 w.set_samples_per_month(3000000)
 
-w.run(mmdtype, hosts=[('localhost', 48)], calls=[('sampling-start.sh', 1), ('coincidence-start.sh', 1), ('sub-start.sh', 1), ('mmd-start.sh', 1)], with_history=True, without_arc=True)
+w.run(mmdtype, hosts=[('localhost', 48)],
+      calls=[('sampling-start.sh', 1), ('coincidence-start.sh', 1), ('sub-start.sh', 1), ('mmd-start.sh', 1)],
+      with_history=True, without_arc=True)
