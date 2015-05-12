f1b76409166b59e8b33f47dabff6eb363fcee785
Ralf Quast <ralf.quast@brockmann-consult.de>
2015-05-12 16:28:29 +0200

    Update

--- mms/src/main/python/l3u.py
+++ mms/src/main/python/l3u.py
@@ -3,6 +3,7 @@ __author__ = 'Ralf Quast'
 from svrworkflow import SvrWorkflow
 
 usecase = 'l3u'
+#version = 'EXP1.2-rc2'
 version = 'v2.1.10'
 archive_root = '/group_workspaces/cems2/esacci_sst/output'
 report_root = '/group_workspaces/cems2/esacci_sst/mms/svr'
