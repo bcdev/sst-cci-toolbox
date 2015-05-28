177f0cf0c2639fa9cf147d2c86d59e016b96b63a
Ralf Quast <ralf.quast@brockmann-consult.de>
2015-05-28 14:10:47 +0200

    Updated constarints

--- mms/src/main/python/mms6a.py
+++ mms/src/main/python/mms6a.py
@@ -11,5 +11,5 @@ w.add_secondary_sensor('amsr2', '2012-07-02', '2015-04-01')
 w.set_samples_per_month(3000000)
 
 w.run(mmdtype, hosts=[('localhost', 48)],
-      calls=[('sampling-start.sh', 1), ('coincidence-start.sh', 1), ('sub-start.sh', 12), ('mmd-start.sh', 1)],
+      calls=[('sampling-start.sh', 1), ('coincidence-start.sh', 4), ('sub-start.sh', 4), ('mmd-start.sh', 4)],
       with_history=True, without_arc=True)
