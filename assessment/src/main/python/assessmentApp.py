380c75ddae21c114adfaf7ea18947b9825a37d07
tom <tom.block@brockmann-consult.de>
2015-07-17 15:46:31 +0200

    can't remember

--- assessment/src/main/python/assessmentApp.py
+++ assessment/src/main/python/assessmentApp.py
@@ -1,13 +1,23 @@
 __author__ = 'tom'
 
-from flask import Flask
+from flask import Flask, render_template
+
 
 class AsessmentApp(Flask):
 
+
     def __init__(self, import_name):
+        super(AsessmentApp, self).__init__(import_name)
         self.name = 'Assessment'
 
+    @app.route('/hello/')
+    @app.route('/hello/<name>')
+    def hello(name=None):
+        return render_template('hello.html', name=name)
+
+
 app = AsessmentApp(__name__)
+
 
 if __name__ == '__main__':
     app.run()
