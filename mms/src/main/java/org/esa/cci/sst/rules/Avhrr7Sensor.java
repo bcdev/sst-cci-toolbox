021b997686f1e7fdde3f20db4ee821d406125f50
tom <tom.block@brockmann-consult.de>
2015-10-09 15:27:27 +0200

    integrated avhrr noaa 7

--- mms/src/main/java/org/esa/cci/sst/rules/Avhrr7Sensor.java
+++ mms/src/main/java/org/esa/cci/sst/rules/Avhrr7Sensor.java
@@ -16,6 +16,8 @@
 
 package org.esa.cci.sst.rules;
 
+import org.esa.cci.sst.tools.Constants;
+
 /**
  * Sets the sensor to 'avhrr.n7'.
  *
@@ -25,6 +27,6 @@ package org.esa.cci.sst.rules;
 final class Avhrr7Sensor extends SensorRule {
 
     Avhrr7Sensor() {
-        super("avhrr.n7");
+        super(Constants.SENSOR_NAME_ORB_AVHRR_N07);
     }
 }
