151d7bb68786013fade39f016eb10aee8d6c13b1
tom <tom.block@brockmann-consult.de>
2015-10-12 14:55:22 +0200

    introduce mms11b_early us case

---
+++ mms/src/main/python/mms11b_early.py
@@ -0,0 +1,42 @@
+from workflow import Period
+from workflow import Workflow
+
+usecase = 'mms11b_early'
+mmdtype = 'mmd11'
+
+w = Workflow(usecase)
+w.add_primary_sensor('avhrr.n07', '1981-09-01', '1985-02-01')
+w.add_primary_sensor('avhrr.n08', '1983-05-04', '1985-10-03')
+#w.add_primary_sensor('avhrr.n09', '1985-02-25', '1988-11-07')
+#w.add_primary_sensor('avhrr.n10', '1986-11-17', '1991-09-17')
+#w.add_primary_sensor('avhrr.n11', '1988-11-08', '1994-09-14')
+#w.add_primary_sensor('avhrr.n12', '1991-09-16', '1998-12-15')
+#w.add_primary_sensor('avhrr.n14', '1995-01-01', '2000-01-01')
+#w.add_primary_sensor('avhrr.n15', '1998-10-26', '2003-04-09')
+#w.add_primary_sensor('avhrr.n15', '2003-12-21', '2011-01-01')
+#w.add_primary_sensor('avhrr.n16', '2001-01-01', '2003-04-09')
+#w.add_primary_sensor('avhrr.n16', '2003-12-21', '2011-01-01')
+#w.add_primary_sensor('avhrr.n17', '2002-07-10', '2003-04-09')
+#w.add_primary_sensor('avhrr.n17', '2003-12-21', '2011-01-01')
+#w.add_primary_sensor('avhrr.n18', '2005-06-05', '2014-01-01')
+#w.add_primary_sensor('avhrr.n19', '2009-02-06', '2014-01-01')
+#w.add_primary_sensor('avhrr.m02', '2006-11-21', '2014-01-01')
+w.add_secondary_sensor('avhrr.n07', '1981-09-01', '1985-02-01')
+w.add_secondary_sensor('avhrr.n08', '1983-05-04', '1985-10-03')
+#w.add_secondary_sensor('avhrr.n09', '1985-02-25', '1988-11-07')
+#w.add_secondary_sensor('avhrr.n10', '1986-11-17', '1991-09-17')
+#w.add_secondary_sensor('avhrr.n11', '1988-11-08', '1994-09-14')
+#w.add_secondary_sensor('avhrr.n12', '1991-09-16', '1998-12-15')
+#w.add_secondary_sensor('avhrr.n14', '1995-01-01', '2000-01-01')
+#w.add_secondary_sensor('avhrr.n15', '1998-10-26', '2003-04-09')
+#w.add_secondary_sensor('avhrr.n15', '2003-12-21', '2011-01-01')
+#w.add_secondary_sensor('avhrr.n16', '2001-01-01', '2003-04-09')
+#w.add_secondary_sensor('avhrr.n16', '2003-12-21', '2011-01-01')
+#w.add_secondary_sensor('avhrr.n17', '2002-07-10', '2003-04-09')
+#w.add_secondary_sensor('avhrr.n17', '2003-12-21', '2010-10-01')
+#w.add_secondary_sensor('avhrr.n18', '2005-06-05', '2014-01-01')
+#w.add_secondary_sensor('avhrr.n19', '2009-02-06', '2014-01-01')
+#w.add_secondary_sensor('avhrr.m02', '2006-11-21', '2014-01-01')
+w.set_samples_per_month(40000000)
+
+w.run(mmdtype, hosts=[('localhost', 48)], selected_only=True)
