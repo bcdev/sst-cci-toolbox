f85eadc8897ff78ba745327015de28787d1f9d2f
tom <tom.block@brockmann-consult.de>
2016-02-01 11:00:16 +0100

    added v1.2 data integration test, refactored test code

--- mms/src/test/java/org/esa/beam/dataio/cci/sst/NcAvhrrGacProductReaderIntegrationTest.java
+++ mms/src/test/java/org/esa/beam/dataio/cci/sst/NcAvhrrGacProductReaderIntegrationTest.java
@@ -37,64 +37,28 @@ public class NcAvhrrGacProductReaderIntegrationTest {
             assertBandCorrect("ch2", 2, 2, 0.002499999936844688, 1e-4, 0.0, "reflectance", product);
             assertBandCorrect("ch3b", 3, 3, -54.529998779296875, 0.01, 273.1499938964844, "kelvin", product);
             assertBandCorrect("ch4", 4, 4, 233.56999478116632, 0.01, 273.1499938964844, "kelvin", product);
+            assertBandCorrect("ch5", 5, 5, -54.53, 0.01, 273.15, "kelvin", product);
 
-            assertCloudBandCorrect(product);
+            assertCloudMaskBandCorrect(product);
 
-            final Band cloudProbabilityBand = product.getBand("cloud_probability");
-            assertNotNull(cloudProbabilityBand);
-            assertEquals(128.0, ProductUtils.getGeophysicalSampleDouble(cloudProbabilityBand, 7, 7, 0), 1e-8);
+            assertBandCorrect("cloud_probability", 7, 7, 128, 1.0, 0.0, null, product);
+            assertBandCorrect("dtime", 8, 8, 0.0, 0.01, 0.0, "seconds", product);
+            assertBandCorrect("ict_temp", 9, 9, 297.49999335221946, 0.01, 273.1499938964844, "kelvin", product);
 
-            final Band dtimeBand = product.getBand("dtime");
-            assertNotNull(dtimeBand);
-            assertEquals("seconds", dtimeBand.getUnit());
-            assertEquals(0.0, ProductUtils.getGeophysicalSampleDouble(dtimeBand, 8, 8, 0), 1e-8);
 
-            final Band latBand = product.getBand("lat");
-            assertNotNull(latBand);
-            assertEquals("degrees_north", latBand.getUnit());
-            assertEquals(48.35000228881836, ProductUtils.getGeophysicalSampleDouble(latBand, 9, 9, 0), 1e-8);
+            assertBandCorrect("lat", 9, 9, 48.35000228881836, 1.0, 0.0, "degrees_north", product);
+            assertBandCorrect("lon", 10, 10, -173.11300659179688, 1.0, 0.0, "degrees_east", product);
 
-            final Band lonBand = product.getBand("lon");
-            assertNotNull(lonBand);
-            assertEquals("degrees_east", lonBand.getUnit());
-            assertEquals(-173.11300659179688, ProductUtils.getGeophysicalSampleDouble(lonBand, 10, 10, 0), 1e-8);
+            assertCorrectFlagsBand(product);
 
-            final Band qualFlagsBand = product.getBand("qual_flags");
-            assertNotNull(qualFlagsBand);
-            assertEquals(0.0, ProductUtils.getGeophysicalSampleDouble(qualFlagsBand, 11, 11, 0), 1e-8);
-            final FlagCoding flagCoding = qualFlagsBand.getFlagCoding();
-            assertNotNull(flagCoding);
-            final String[] flagNames = flagCoding.getFlagNames();
-            assertArrayEquals(new String[]{"bad_navigation", "bad_calibration", "bad_timing", "missing_line", "bad_data"}, flagNames);
-            assertEquals(1, flagCoding.getFlagMask("bad_navigation"));
-            assertEquals(2, flagCoding.getFlagMask("bad_calibration"));
-            assertEquals(4, flagCoding.getFlagMask("bad_timing"));
-            assertEquals(8, flagCoding.getFlagMask("missing_line"));
-            assertEquals(16, flagCoding.getFlagMask("bad_data"));
+            assertBandCorrect("relative_azimuth_angle", 12, 12, -327.67999267578125, 0.01, 0.0, "angular_degree", product);
+            assertBandCorrect("satellite_zenith_angle", 13, 13, 61.869998617097735, 0.01, 0.0, "angular_degree", product);
+            assertBandCorrect("solar_zenith_angle", 14, 14, 119.24999733455479, 0.01, 0.0, "angular_degree", product);
 
-            final Band relativeAzimuthAngleBand = product.getBand("relative_azimuth_angle");
-            assertNotNull(relativeAzimuthAngleBand);
-            assertEquals("angular_degree", relativeAzimuthAngleBand.getUnit());
-            assertEquals(0.01, relativeAzimuthAngleBand.getScalingFactor(), 1e-8);
-            assertEquals(-327.67999267578125, ProductUtils.getGeophysicalSampleDouble(relativeAzimuthAngleBand, 12, 12, 0), 1e-8);
-
-            final Band satelliteZenithAngleBand = product.getBand("satellite_zenith_angle");
-            assertNotNull(satelliteZenithAngleBand);
-            assertEquals("angular_degree", satelliteZenithAngleBand.getUnit());
-            assertEquals(0.01, satelliteZenithAngleBand.getScalingFactor(), 1e-8);
-            assertEquals(61.869998617097735, ProductUtils.getGeophysicalSampleDouble(satelliteZenithAngleBand, 13, 13, 0), 1e-8);
-
-            final Band solarZenithAngleBand = product.getBand("solar_zenith_angle");
-            assertNotNull(solarZenithAngleBand);
-            assertEquals("angular_degree", solarZenithAngleBand.getUnit());
-            assertEquals(0.01, solarZenithAngleBand.getScalingFactor(), 1e-8);
-            assertEquals(119.24999733455479, ProductUtils.getGeophysicalSampleDouble(solarZenithAngleBand, 14, 14, 0), 1e-8);
         } finally {
             product.dispose();
         }
     }
-
-
 
     @Test
     public void testReadL1B_v_1_2_TestProduct() throws Exception {
@@ -109,13 +73,42 @@ public class NcAvhrrGacProductReaderIntegrationTest {
             assertBandCorrect("ch2", 2, 2, 0.20239999488694593, 1e-4, 0.0, "reflectance", product);
             assertBandCorrect("ch3b", 3, 3, 289.92999352142215, 0.01, 273.1499938964844, "kelvin", product);
             assertBandCorrect("ch4", 4, 4, 269.36999398097396, 0.01, 273.1499938964844, "kelvin", product);
+            assertBandCorrect("ch5", 5, 5, 269.6799939740449, 0.01, 273.1499938964844, "kelvin", product);
 
-            assertCloudBandCorrect(product);
+            assertCloudMaskBandCorrect(product);
 
             assertBandCorrect("cloud_probability", 5, 5, -0.00393599271774292, 0.0039369999431073666, 0.5, null, product);
+            assertBandCorrect("dtime", 6, 6, 3.000640869140625, 1.0, 0.0, "seconds", product);
+            assertBandCorrect("ict_temp", 7, 7, 288.7099935486913, 0.01, 273.1499938964844, "kelvin", product);
+
+            assertBandCorrect("lat", 7, 7, 47.566001892089844, 1.0, 0.0, "degrees_north", product);
+            assertBandCorrect("lon", 8, 8, -174.86199951171875, 1.0, 0.0, "degrees_east", product);
+
+            // @todo 1 tb/tb wait for Claires answer and adapt 2016-02-01
+            //assertCorrectFlagsBand(product);
+
+            assertBandCorrect("relative_azimuth_angle", 10, 10, -327.67999267578125, 0.01, 0.0, "angular_degree", product);
+            assertBandCorrect("satellite_zenith_angle", 11, 11, 63.6699985768646, 0.01, 0.0, "angular_degree", product);
+            assertBandCorrect("solar_zenith_angle", 12, 12, 50.019998881965876, 0.01, 0.0, "angular_degree", product);
         } finally {
             product.dispose();
         }
+    }
+
+
+    private void assertCorrectFlagsBand(Product product) {
+        final Band qualFlagsBand = product.getBand("qual_flags");
+        assertNotNull(qualFlagsBand);
+        assertEquals(0.0, ProductUtils.getGeophysicalSampleDouble(qualFlagsBand, 11, 11, 0), 1e-8);
+        final FlagCoding flagCoding = qualFlagsBand.getFlagCoding();
+        assertNotNull(flagCoding);
+        final String[] flagNames = flagCoding.getFlagNames();
+        assertArrayEquals(new String[]{"bad_navigation", "bad_calibration", "bad_timing", "missing_line", "bad_data"}, flagNames);
+        assertEquals(1, flagCoding.getFlagMask("bad_navigation"));
+        assertEquals(2, flagCoding.getFlagMask("bad_calibration"));
+        assertEquals(4, flagCoding.getFlagMask("bad_timing"));
+        assertEquals(8, flagCoding.getFlagMask("missing_line"));
+        assertEquals(16, flagCoding.getFlagMask("bad_data"));
     }
 
     private void assertBandCorrect(String name, int x, int y, double value, double scaleFactor, double scaleOffset, String unit, Product product) {
@@ -127,7 +120,7 @@ public class NcAvhrrGacProductReaderIntegrationTest {
         assertEquals(value, ProductUtils.getGeophysicalSampleDouble(band, x, y, 0), 1e-8);
     }
 
-    private void assertCloudBandCorrect(Product product) {
+    private void assertCloudMaskBandCorrect(Product product) {
         final Band cloudMaskBand = product.getBand("cloud_mask");
         assertNotNull(cloudMaskBand);
         assertEquals(7.0, ProductUtils.getGeophysicalSampleDouble(cloudMaskBand, 6, 6, 0), 1e-8);
