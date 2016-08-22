c5e882ae4b9fdd167c06edac525d3fb0fe375664
sabine <sabine.embacher@brockmann-consult.de>
2016-08-22 15:51:47 +0200

    install script

--- install_car_web_tool.sh
+++ install_car_web_tool.sh
@@ -2,5 +2,5 @@ git pull -f
 git checkout -f
 
 rm -rf /home/cartool/car-web-tool/*
-cp -r car_web/* /home/cartool/car-web-tool
+cp -r car-web/* /home/cartool/car-web-tool
 chmod +x /home/cartool/car-web-tool/run_server_port5000.sh
