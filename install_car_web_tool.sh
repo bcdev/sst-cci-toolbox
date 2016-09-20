ebf4752b969ff32488f2a2864cfdea14deea947e
sabine <sabine.embacher@brockmann-consult.de>
2016-09-20 09:52:50 +0200

    chmod

--- install_car_web_tool.sh
+++ install_car_web_tool.sh
@@ -4,3 +4,4 @@ git checkout -f
 rm -rf /home/cartool/car-web-tool/*
 cp -r car-web/* /home/cartool/car-web-tool
 chmod +x /home/cartool/car-web-tool/run_server_port5000.sh
+chmod +x install_car_web_tool.sh
