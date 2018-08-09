0690bc70d2b52ee6c6a2f60d1e3492445dffe5ca
tom <tom.block@brockmann-consult.de>
2018-08-09 17:03:55 +0200

    continued integrate version awareness

--- quality-assessment/cci/sst/qa/product_type.py
+++ quality-assessment/cci/sst/qa/product_type.py
@@ -2,170 +2,103 @@ class ProductType:
     def __init__(self, variable_names, geophysical_check_spec, mask_consistency_check_specs, sst_variable_names):
         """
 
-        :type variable_names: list
-        :type geophysical_check_spec: list
-        :type mask_consistency_check_specs: list
-        :type sst_variable_names: list
+        :type variable_names: dict
+        :type geophysical_check_spec: dict
+        :type mask_consistency_check_specs: dict
+        :type sst_variable_names: dict
         """
         self.variable_names = variable_names
         self.geophysical_check_spec = geophysical_check_spec
         self.mask_consistency_check_specs = mask_consistency_check_specs
         self.sst_variable_names = sst_variable_names
 
-    def get_variable_names(self):
+    def get_variable_names(self, version):
         """
 
         :rtype : list
         """
-        return self.variable_names
+        return self.variable_names[version]
 
-    def get_geophysical_check_spec(self):
+    def get_geophysical_check_spec(self, version):
         """
 
         :rtype : list
         """
-        return self.geophysical_check_spec
+        return self.geophysical_check_spec[version]
 
-    def get_mask_consistency_check_specs(self):
+    def get_mask_consistency_check_specs(self, version):
         """
 
         :rtype : list
         """
-        return self.mask_consistency_check_specs
+        return self.mask_consistency_check_specs[version]
 
-    def get_sst_variable_names(self):
+    def get_sst_variable_names(self, version):
         """
 
         :rtype : list
         """
-        return self.sst_variable_names
+        return self.sst_variable_names[version]
 
     def set_version(self, version):
         self.version = version
 
 
 class L2P(ProductType):
+    l2_variable_names = {
+        "1.2": ['adjustment_uncertainty', 'l2p_flags', 'large_scale_correlated_uncertainty', 'lat', 'lon', 'quality_level', 'sea_surface_temperature', 'sea_surface_temperature_depth', 'sses_bias',
+                'sses_standard_deviation', 'sst_depth_total_uncertainty', 'sst_dtime', 'synoptically_correlated_uncertainty', 'time', 'uncorrelated_uncertainty', 'wind_speed'],
+        "2.0": ["adjustment_uncertainty", "aerosol_dynamic_indicator", "l2p_flags", "large_scale_correlated_uncertainty", "lat", "lon", "probability_clear", "quality_level", "sea_surface_temperature",
+                "sea_surface_temperature_depth", "sensitivity", "sses_bias", "sses_standard_deviation", "sst_depth_dtime", "sst_depth_total_uncertainty", "sst_dtime",
+                "synoptically_correlated_uncertainty", "time", "uncorrelated_uncertainty", "wind_speed"]}
+
+    l2_geophysical_checks = {"1.2": ['sea_surface_temperature', 'sea_surface_temperature_depth', -5.0, 10.0]}
+
+    l2_mask_checks = {"1.2": [['sea_surface_temperature', 'quality_level', 'quality_level', [0, 2, 3, 4, 5]], ['sea_surface_temperature', 'sses_bias', 'quality_level', range(0, 6)],
+                              ['sea_surface_temperature', 'sses_standard_deviation', 'quality_level', range(0, 6)],
+                              ['sea_surface_temperature', 'large_scale_correlated_uncertainty', 'quality_level', range(0, 6)],
+                              ['sea_surface_temperature', 'synoptically_correlated_uncertainty', 'quality_level', range(0, 6)],
+                              ['sea_surface_temperature', 'uncorrelated_uncertainty', 'quality_level', range(0, 6)],
+                              ['sea_surface_temperature', 'adjustment_uncertainty', 'quality_level', range(0, 6)],
+                              ['sea_surface_temperature', 'sea_surface_temperature_depth', 'quality_level', range(0, 6)],
+                              ['sea_surface_temperature_depth', 'sst_depth_total_uncertainty', 'quality_level', range(0, 6)]]}
+
+    l2_sst_variable_names = {"1.2": ['sea_surface_temperature', 'sea_surface_temperature_depth']}
+
     def __init__(self):
-        ProductType.__init__(self,
-                             [
-                                 'adjustment_uncertainty',
-                                 'l2p_flags',
-                                 'large_scale_correlated_uncertainty',
-                                 'lat',
-                                 'lon',
-                                 'quality_level',
-                                 'sea_surface_temperature',
-                                 'sea_surface_temperature_depth',
-                                 'sses_bias',
-                                 'sses_standard_deviation',
-                                 'sst_depth_total_uncertainty',
-                                 'sst_dtime',
-                                 'synoptically_correlated_uncertainty',
-                                 'time',
-                                 'uncorrelated_uncertainty',
-                                 'wind_speed',
-                             ],
-                             ['sea_surface_temperature', 'sea_surface_temperature_depth', -5.0, 10.0],
-                             [
-                                 ['sea_surface_temperature', 'quality_level', 'quality_level',
-                                  [0, 2, 3, 4, 5]],
-                                 ['sea_surface_temperature', 'sses_bias', 'quality_level',
-                                  range(0, 6)],
-                                 ['sea_surface_temperature', 'sses_standard_deviation', 'quality_level',
-                                  range(0, 6)],
-                                 ['sea_surface_temperature', 'large_scale_correlated_uncertainty', 'quality_level',
-                                  range(0, 6)],
-                                 ['sea_surface_temperature', 'synoptically_correlated_uncertainty', 'quality_level',
-                                  range(0, 6)],
-                                 ['sea_surface_temperature', 'uncorrelated_uncertainty', 'quality_level',
-                                  range(0, 6)],
-                                 ['sea_surface_temperature', 'adjustment_uncertainty', 'quality_level',
-                                  range(0, 6)],
-                                 ['sea_surface_temperature', 'sea_surface_temperature_depth', 'quality_level',
-                                  range(0, 6)],
-                                 ['sea_surface_temperature_depth', 'sst_depth_total_uncertainty', 'quality_level',
-                                  range(0, 6)],
-                             ],
-                             [
-                                 'sea_surface_temperature',
-                                 'sea_surface_temperature_depth'
-                             ])
+        ProductType.__init__(self, self.l2_variable_names, self.l2_geophysical_checks, self.l2_mask_checks, self.l2_sst_variable_names)
 
 
 class L3U(ProductType):
+    l3_variable_names = {"1.2": ['lat', 'lat_bnds', 'lon', 'lon_bnds', 'time', 'time_bnds', 'sst_dtime', 'sea_surface_temperature', 'sea_surface_temperature_depth', 'wind_speed', 'quality_level',
+                                 'sses_standard_deviation', 'sst_depth_total_uncertainty', 'large_scale_correlated_uncertainty', 'synoptically_correlated_uncertainty', 'uncorrelated_uncertainty',
+                                 'adjustment_uncertainty', 'l2p_flags', 'sses_bias', 'aerosol_dynamic_indicator', 'sensitivity']}
+
+    l3_geophysical_checks = {"1.2": ['sea_surface_temperature', 'sea_surface_temperature_depth', -5.0, 10.0]}
+
+    l3_mask_checks = {"1.2": [['sea_surface_temperature', 'quality_level', 'quality_level', [0, 2, 3, 4, 5]], ['sea_surface_temperature', 'sses_bias', 'quality_level', range(0, 6)],
+                              ['sea_surface_temperature', 'sses_standard_deviation', 'quality_level', range(0, 6)],
+                              ['sea_surface_temperature', 'large_scale_correlated_uncertainty', 'quality_level', range(0, 6)],
+                              ['sea_surface_temperature', 'synoptically_correlated_uncertainty', 'quality_level', range(0, 6)],
+                              ['sea_surface_temperature', 'uncorrelated_uncertainty', 'quality_level', range(0, 6)],
+                              ['sea_surface_temperature', 'adjustment_uncertainty', 'quality_level', range(0, 6)],
+                              ['sea_surface_temperature', 'sea_surface_temperature_depth', 'quality_level', range(0, 6)],
+                              ['sea_surface_temperature_depth', 'sst_depth_total_uncertainty', 'quality_level', range(0, 6)]]}
+
+    l3_sst_variable_names = {"1.2": ['sea_surface_temperature', 'sea_surface_temperature_depth']}
+
     def __init__(self):
-        ProductType.__init__(self,
-                             [
-                                 'lat',
-                                 'lat_bnds',
-                                 'lon',
-                                 'lon_bnds',
-                                 'time',
-                                 'time_bnds',
-                                 'sst_dtime',
-                                 'sea_surface_temperature',
-                                 'sea_surface_temperature_depth',
-                                 'wind_speed',
-                                 'quality_level',
-                                 'sses_standard_deviation',
-                                 'sst_depth_total_uncertainty',
-                                 'large_scale_correlated_uncertainty',
-                                 'synoptically_correlated_uncertainty',
-                                 'uncorrelated_uncertainty',
-                                 'adjustment_uncertainty',
-                                 'l2p_flags',
-                                 'sses_bias',
-                                 'aerosol_dynamic_indicator',
-                                 'sensitivity'
-                             ],
-                             ['sea_surface_temperature', 'sea_surface_temperature_depth', -5.0, 10.0],
-                             [
-                                 ['sea_surface_temperature', 'quality_level', 'quality_level',
-                                  [0, 2, 3, 4, 5]],
-                                 ['sea_surface_temperature', 'sses_bias', 'quality_level',
-                                  range(0, 6)],
-                                 ['sea_surface_temperature', 'sses_standard_deviation', 'quality_level',
-                                  range(0, 6)],
-                                 ['sea_surface_temperature', 'large_scale_correlated_uncertainty', 'quality_level',
-                                  range(0, 6)],
-                                 ['sea_surface_temperature', 'synoptically_correlated_uncertainty', 'quality_level',
-                                  range(0, 6)],
-                                 ['sea_surface_temperature', 'uncorrelated_uncertainty', 'quality_level',
-                                  range(0, 6)],
-                                 ['sea_surface_temperature', 'adjustment_uncertainty', 'quality_level',
-                                  range(0, 6)],
-                                 ['sea_surface_temperature', 'sea_surface_temperature_depth', 'quality_level',
-                                  range(0, 6)],
-                                 ['sea_surface_temperature_depth', 'sst_depth_total_uncertainty', 'quality_level',
-                                  range(0, 6)]
-                             ],
-                             [
-                                 'sea_surface_temperature',
-                                 'sea_surface_temperature_depth'
-                             ])
+        ProductType.__init__(self, self.l3_variable_names, self.l3_geophysical_checks, self.l3_mask_checks, self.l3_sst_variable_names)
 
 
 class L4(ProductType):
+    l4_variable_names = {"1.2": ['lat', 'lat_bnds', 'lon', 'lon_bnds', 'time', 'time_bnds', 'analysed_sst', 'sea_ice_fraction', 'quality_level', 'analysis_error', 'sea_ice_fraction_error', 'mask', ]}
+
+    l4_geophysical_checks = {"1.2": []}
+
+    l4_mask_checks = {"1.2": [['analysed_sst', 'analysis_error', None], ['sea_ice_fraction', 'sea_ice_fraction_error', None]]}
+
+    l4_sst_variable_names = {"1.2": ['analysed_sst']}
+
     def __init__(self):
-        ProductType.__init__(self,
-                             [
-                                 'lat',
-                                 'lat_bnds',
-                                 'lon',
-                                 'lon_bnds',
-                                 'time',
-                                 'time_bnds',
-                                 'analysed_sst',
-                                 'sea_ice_fraction',
-                                 'quality_level'
-                                 'analysis_error',
-                                 'sea_ice_fraction_error',
-                                 'mask',
-                             ], [],
-                             [
-                                 ['analysed_sst', 'analysis_error', None],
-                                 ['sea_ice_fraction', 'sea_ice_fraction_error', None]
-                             ],
-                             [
-                                 'analysed_sst'
-                             ])
+        ProductType.__init__(self, self.l4_variable_names, self.l4_geophysical_checks, self.l4_mask_checks, self.l4_sst_variable_names)
--- quality-assessment/test/cci/sst/qa/product_type_test.py
+++ quality-assessment/test/cci/sst/qa/product_type_test.py
@@ -1,27 +1,28 @@
 import unittest
 
 from cci.sst.qa.product_type import L2P
+from cci.sst.qa.product_type import L3U
+from cci.sst.qa.product_type import L4
 
 
 class ProductTypeTest(unittest.TestCase):
     def test_get_variable_names_L2P_v1_2(self):
         product_type = L2P()
 
-        # product_type.setVersion("1.2")
-        # @todo 1 tb/tb continue here
+        version = "1.2"
 
-        variable_names = product_type.get_variable_names()
+        variable_names = product_type.get_variable_names(version)
         self.assertEqual(16, len(variable_names))
         self.assertEqual("adjustment_uncertainty", variable_names[0])
         self.assertEqual("quality_level", variable_names[5])
         self.assertEqual("sst_depth_total_uncertainty", variable_names[10])
         self.assertEqual("wind_speed", variable_names[15])
 
-        geophysical_spec = product_type.get_geophysical_check_spec()
+        geophysical_spec = product_type.get_geophysical_check_spec(version)
         self.assertEqual(4, len(geophysical_spec))
         self.assertEqual("sea_surface_temperature_depth", geophysical_spec[1])
 
-        mask_specs = product_type.get_mask_consistency_check_specs()
+        mask_specs = product_type.get_mask_consistency_check_specs(version)
         self.assertEqual(9, len(mask_specs))
         mask_spec = mask_specs[2]
         self.assertEqual("sea_surface_temperature", mask_spec[0])
@@ -29,10 +30,77 @@ class ProductTypeTest(unittest.TestCase):
         self.assertEqual("quality_level", mask_spec[2])
         self.assertEqual(range(0, 6), mask_spec[3])
 
-        sst_variable_names = product_type.get_sst_variable_names()
+        sst_variable_names = product_type.get_sst_variable_names(version)
         self.assertEqual(2, len(sst_variable_names))
         self.assertEqual("sea_surface_temperature", sst_variable_names[0])
         self.assertEqual("sea_surface_temperature_depth", sst_variable_names[1])
+
+    def test_get_variable_names_L2P_v2_0(self):
+        product_type = L2P()
+
+        version = "2.0"
+
+        variable_names = product_type.get_variable_names(version)
+        self.assertEqual(20, len(variable_names))
+        self.assertEqual("aerosol_dynamic_indicator", variable_names[1])
+        self.assertEqual("probability_clear", variable_names[6])
+        self.assertEqual("sses_bias", variable_names[11])
+        self.assertEqual("synoptically_correlated_uncertainty", variable_names[16])
+
+        # @todo 1 tb/tb continue here 2018-08-09
+
+    def test_get_variable_names_L3U_v1_2(self):
+        product_type = L3U()
+
+        version = "1.2"
+
+        variable_names = product_type.get_variable_names(version)
+        self.assertEqual(21, len(variable_names))
+        self.assertEqual("lat_bnds", variable_names[1])
+        self.assertEqual("sst_dtime", variable_names[6])
+        self.assertEqual("sses_standard_deviation", variable_names[11])
+        self.assertEqual("adjustment_uncertainty", variable_names[16])
+
+        geophysical_spec = product_type.get_geophysical_check_spec(version)
+        self.assertEqual(4, len(geophysical_spec))
+        self.assertEqual(-5, geophysical_spec[2])
+
+        mask_specs = product_type.get_mask_consistency_check_specs(version)
+        self.assertEqual(9, len(mask_specs))
+        mask_spec = mask_specs[3]
+        self.assertEqual("sea_surface_temperature", mask_spec[0])
+        self.assertEqual("large_scale_correlated_uncertainty", mask_spec[1])
+        self.assertEqual("quality_level", mask_spec[2])
+        self.assertEqual(range(0, 6), mask_spec[3])
+
+        sst_variable_names = product_type.get_sst_variable_names(version)
+        self.assertEqual(2, len(sst_variable_names))
+        self.assertEqual("sea_surface_temperature", sst_variable_names[0])
+        self.assertEqual("sea_surface_temperature_depth", sst_variable_names[1])
+
+    def test_get_variable_names_L4_v1_2(self):
+        product_type = L4()
+
+        version = "1.2"
+
+        variable_names = product_type.get_variable_names(version)
+        self.assertEqual(12, len(variable_names))
+        self.assertEqual("lon", variable_names[2])
+        self.assertEqual("sea_ice_fraction", variable_names[7])
+
+        geophysical_spec = product_type.get_geophysical_check_spec(version)
+        self.assertEqual(0, len(geophysical_spec))
+
+        mask_specs = product_type.get_mask_consistency_check_specs(version)
+        self.assertEqual(2, len(mask_specs))
+        mask_spec = mask_specs[0]
+        self.assertEqual("analysed_sst", mask_spec[0])
+        self.assertEqual("analysis_error", mask_spec[1])
+        self.assertEqual(None, mask_spec[2])
+
+        sst_variable_names = product_type.get_sst_variable_names(version)
+        self.assertEqual(1, len(sst_variable_names))
+        self.assertEqual("analysed_sst", sst_variable_names[0])
 
 
 if __name__ == '__main__':
