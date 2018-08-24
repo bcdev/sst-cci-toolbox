4bbc95c81d52afbad751edd5c24ea163d9a85131
tom <tom.block@brockmann-consult.de>
2018-08-24 14:21:08 +0200

    added simple test

--- quality-assessment/test/cci/sst/qa/reportaccumulator_test.py
+++ quality-assessment/test/cci/sst/qa/reportaccumulator_test.py
@@ -9,13 +9,14 @@ class ReportAccumulatorTests(unittest.TestCase):
     def test_accumulate(self):
         data_dir = TestDataUtils.get_test_data_dir()
 
-        accumulator = ReportAccumulator(data_dir, report_filename_pattern="report.*\\.json")
+        accumulator = ReportAccumulator(data_dir, report_filename_pattern="report_l2p.*\\.json")
 
         accumulator.accumulate()
 
-        self.assertEqual(31874970.0, accumulator.summary_report["not_ocean"])
-        self.assertEqual(3162407.0, accumulator.summary_report["adjustment_uncertainty.count.valid"])
-        self.assertEqual(25530328.0, accumulator.summary_report["lat.count.total"])
-        self.assertEqual(51446728.0, accumulator.summary_report["sea_surface_temperature.count.total"])
+        self.assertEqual(6069611.0, accumulator.summary_report["not_ocean"])
+        self.assertEqual(3048183.0, accumulator.summary_report["adjustment_uncertainty.count.valid"])
+        self.assertEqual(25526728.0, accumulator.summary_report["lat.count.total"])
+        self.assertEqual(25526728.0, accumulator.summary_report["lon.count.total"])
+        self.assertEqual(25526728.0, accumulator.summary_report["sea_surface_temperature.count.total"])
 
         
