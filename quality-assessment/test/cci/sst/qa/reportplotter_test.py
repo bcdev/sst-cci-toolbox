0a843042b9317ff4f482dba9cf8f525da44bd979
tom <tom.block@brockmann-consult.de>
2019-03-21 16:56:08 +0100

    intermediate plotting update

--- quality-assessment/test/cci/sst/qa/reportplotter_test.py
+++ quality-assessment/test/cci/sst/qa/reportplotter_test.py
@@ -10,24 +10,67 @@ from test.cci.sst.qa.test_data_utils import TestDataUtils
 
 
 class ReportPlotterTests(unittest.TestCase):
-    # @todo 1 tb/tb reactivate this 2019-03-20
-    # def test_plot_report_AVHRR(self):
-    #     data_dir = TestDataUtils.get_test_data_dir()
-    #     report_file = os.path.join(data_dir, "l2p-AVHRR16_G-summary.json")
-    #     plotter = ReportPlotter('l2p', 'AVHRR16_G', ProductVerifier.load_report(report_file))
-    #     plotter.plot()
-    #
-    # def test_plot_report_AATSR(self):
-    #     data_dir = TestDataUtils.get_test_data_dir()
-    #     report_file = os.path.join(data_dir, "l2p-AATSR-summary.json")
-    #     plotter = ReportPlotter('l2p', 'AATSR', ProductVerifier.load_report(report_file))
-    #     plotter.plot()
-    #
-    # def test_plot_report_L3U_v2_1(self):
-    #     data_dir = TestDataUtils.get_test_data_dir()
-    #     report_file = os.path.join(data_dir, "l3u_AVHRR17G_v21_summary.json")
-    #     plotter = ReportPlotter('l3u', 'AVHRR17_G', ProductVerifier.load_report(report_file))
-    #     plotter.plot()
+    def test_plot_report_AVHRR(self):
+        data_dir = TestDataUtils.get_test_data_dir()
+        report_file = os.path.join(data_dir, "l2p-AVHRR16_G-summary.json")
+        plotter = ReportPlotter('l2p', 'AVHRR16_G', ProductVerifier.load_report(report_file))
+        plotter.plot()
+
+    def test_plot_report_AATSR(self):
+        data_dir = TestDataUtils.get_test_data_dir()
+        report_file = os.path.join(data_dir, "l2p-AATSR-summary.json")
+        plotter = ReportPlotter('l2p', 'AATSR', ProductVerifier.load_report(report_file))
+        plotter.plot()
+
+    def test_plot_report_L3U_v2_1(self):
+        data_dir = TestDataUtils.get_test_data_dir()
+        report_file = os.path.join(data_dir, "l3u_AVHRR17G_v21_summary.json")
+        plotter = ReportPlotter('l3u', 'AVHRR17_G', ProductVerifier.load_report(report_file))
+        plotter.plot()
+
+    def test_extract_labels_plot_1_one_label(self):
+        report = {"adjustment_uncertainty.count.total": 20602368.0,
+                  "adjustment_uncertainty.count.valid": 2623244.0,
+                  "adjustment_uncertainty.existence_check": 0.0,
+                  "adjustment_uncertainty.valid_max_check": 0.0,
+                  "adjustment_uncertainty.valid_min_check": 0.0}
+
+        labels = ReportPlotter._extract_labels_plot_1(report)
+        self.assertIsNotNone(labels)
+        self.assertEqual(1, len(labels))
+        self.assertEqual("Adjustment Unc Exists", labels[0])
+
+    def test_extract_labels_plot_1_more_labels(self):
+        report = {"adjustment_uncertainty.count.total": 20602368.0,
+                  "adjustment_uncertainty.count.valid": 2623244.0,
+                  "adjustment_uncertainty.existence_check": 0.0,
+                  "aerosol_dynamic_indicator.existence_check": 0.0,
+                  "aerosol_dynamic_indicator.valid_max_check": 0.0,
+                  "large_scale_correlated_uncertainty.existence_check": 0.0,
+                  "large_scale_correlated_uncertainty.mask_false_negative_check_0": 0.0,
+                  "probability_clear.existence_check": 0.0,
+                  "probability_clear.valid_max_check": 0.0,
+                  "sensitivity.existence_check": 0.0,
+                  "sensitivity.mask_false_negative_check_0": 0.0,
+                  "sensitivity.mask_false_negative_check_1": 39420.0,
+                  "sses_bias.existence_check": 0.0,
+                  "sses_bias.mask_false_negative_check_0": 0.0,
+                  "sses_bias.mask_false_negative_check_1": 39420.0,
+                  "sses_bias.mask_false_negative_check_1_failed_for": "20050606101532-ESACCI-L2P_GHRSST-SSTskin-AATSR-CDR2.0-v02.0-fv01.0.nc",
+                  "sst_dtime.count.total": 20602368.0,
+                  "sst_dtime.count.valid": 20602368.0,
+                  "sst_dtime.existence_check": 0.0}
+
+        labels = ReportPlotter._extract_labels_plot_1(report)
+        self.assertIsNotNone(labels)
+        self.assertEqual(7, len(labels))
+        self.assertEqual("SST DTime Exists", labels[0])
+        self.assertEqual("SSES Bias Exists", labels[1])
+        self.assertEqual("Large Scale Unc Exists", labels[2])
+        self.assertEqual("SST Sens Exists", labels[3])
+        self.assertEqual("Adjustment Unc Exists", labels[4])
+        self.assertEqual("Aerosol Dyn Ind Exists", labels[5])
+        self.assertEqual("Probability Clear Exists", labels[6])
 
     def tearDown(self):
         if os.path.isfile("l2p-AATSR-figure1.pdf"):
